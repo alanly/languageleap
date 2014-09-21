b6063961d6cdc432ecdf95ddf584589bc647babe
Dror Ozgaon <dror.ozgaon@gmail.com>
2014-09-21 14:51:56 -0400

    Added a new layout for the selection menu with nice filters and such.

--- Prototype/home.php
+++ Prototype/home.php
@@ -12,12 +12,20 @@
 
     <!-- CSS -->
 	<link rel="stylesheet" href="CSS/main.css">
+	<link rel="stylesheet" href="Filter/css/filtrify.css">
+	<link rel="stylesheet" href="Filter/css/style.css">
+	<link rel="stylesheet" href="Filter/css/sunburst.css">
 
 	<!-- jQuery -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script type="text/javascript" src="Accordion/js/jquery.browser.js" /></script>
 	<script type="text/javascript" src="Accordion/js/jquery.evoslider.lite-1.1.0.js"></script>
 	<script type="text/javascript" src="Accordion/js/jquery.easing.1.3.js"></script>
+
+	<!-- Videos -->
+	<script type="text/javascript" src="Filter/js/filtrify.min.js"></script>
+	<script type="text/javascript" src="Filter/js/highlight.pack.js"></script>
+	<script type="text/javascript" src="Filter/js/script.js"></script>
 
 </head>
 <body>
--- Prototype/select.php
+++ Prototype/select.php
@@ -12,26 +12,29 @@
     
 	    <dt>Media</dt>
 	    <dd>
-	       	<div class="list-group">
-				<button type="button" class="btn btn-default">Movies</button>
-				<br />
-			  	<button type="button" class="btn btn-default">TV Shows</button>
-			  	<br />
-			  	<button type="button" class="btn btn-default">Commercials</button>
-			  	<br />
-			</div>	        
+	    	<div id="main" role="main">
+			    <div id="content" class="demo">
+						<ul id="containerMedia">
+						    <li><strong>Movies</strong><img id="movies" src="Filter/img/film.png"></li>
+						    <li><strong>TV Shows</strong><img id="tvShows" src="Filter/img/film.png"></li>
+						    <li><strong>Commercials</strong><img id="commercials" src="Filter/img/film.png"></li>
+						</ul>    
+				</div>
+			</div>
 	    </dd>
-	
+
 	    <dt>Commercials</dt>
 	    <dd>
-	    	<div class="list-group">
-	       		<button type="button" class="btn btn-default">Amazon Fire Phone</button>
-	       		<br />
-			  	<button type="button" class="btn btn-default">Samsung Galaxy S5</button>
-			  	<br />
-			  	<button type="button" class="btn btn-default">Apple iPhone 6</button>  
-			  	<br />
-			</div>     
+	    	<div id="main" role="main">
+		    	<div id="content" class="demo" style="overflow:auto;">
+			       	<div id="placeHolder"></div>
+						<ul id="container">
+						    <li data-genre="Comedy" data-main-actors="John Di Girolamo, Mike Lavoie, Alan Ly" data-director="Amazon"><strong>Amazon Fire Phone</strong><img id="firePhone" src="Filter/img/film.png"><span>Genre: <i>Comedy</i></span><span>Actors: <i>John Di Girolamo, Mike Lavoie, Alan Ly</i></span><span>Director: <i>Amazon</i></span></li>
+						    <li data-genre="Sci-Fi" data-main-actors="Quang Tran, Kwok-Chak Wan" data-director="Samsung"><strong>Samsung Galaxy S5</strong><img id="galaxyS5" src="Filter/img/film.png"><span>Genre: <i>Sci-Fi</i></span><span>Actors: <i>Quang Tran, Kwok-Chak Wan</i></span><span>Director: <i>Samsung</i></span></li>
+						    <li data-genre="Thriller" data-main-actors="Dror Ozgaon, David Siekut, Thomas Rahn" data-director="Apple"><strong>Apple iPhone 6</strong><img id="iPhone6" src="Filter/img/film.png"><span>Genre: <i>Thriller</i></span><span>Actors: <i>Dror Ozgaon, David Siekut, Thomas Rahn</i></span><span>Director: <i>Apple</i></span></li>
+						</ul>    
+				</div>
+			</div>   
 	    </dd>
 	
 	    <dt>Season</dt>
@@ -59,12 +62,12 @@
             
     $("#mySlider").evoSlider({
         mode: "accordion",                  // Sets slider mode ("accordion", "slider", or "scroller")
-        width: 960,                         // The width of slider
-        height: 360,                        // The height of slider
+        width: 1460,                         // The width of slider
+        height: 430,                        // The height of slider
         slideSpace: 4,                      // The space between slides
     
-        mouse: true,                        // Enables mousewheel scroll navigation
-        keyboard: true,                     // Enables keyboard navigation (left and right arrows)
+        mouse: false,                        // Enables mousewheel scroll navigation
+        keyboard: false,                     // Enables keyboard navigation (left and right arrows)
         speed: 500,                         // Slide transition speed in ms. (1s = 1000ms)
         easing: "swing",                    // Defines the easing effect mode
         loop: true,                         // Rotate slideshow
@@ -79,6 +82,22 @@
     
         controlNav: true,                   // Enables control navigation
         controlNavAutoHide: false           // Shows control navigation on mouseover and hide it when mouseout 
-    });                                
+    });      
+
+    $(function() {
+		$.filtrify("container", "placeHolder");
+	});
+
+	$("#commercials").click(function() {
+
+		$("#commercials").height(84);
+		$("#commercials").css('align', 'middle');
+	});
+
+	$("#firePhone").click(function() {
+
+		$("#firePhone").height(84);
+		$("#firePhone").css('align', 'middle');
+	});                         
     
 </script>
