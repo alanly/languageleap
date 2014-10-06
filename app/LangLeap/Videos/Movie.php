de07acc02fe0fc357d39f34467a7222fbe6e41a1
Alan Ly <me@alanly.ca>
2014-10-06 14:20:35 -0400

    Remove useless toResponseArray function. [ci skip]

--- app/LangLeap/Videos/Movie.php
+++ app/LangLeap/Videos/Movie.php
@@ -12,19 +12,5 @@ class Movie extends Eloquent implements Billable {
 	{
 		return $this->morphMany('LangLeap\Videos\Video','viewable');
 	}
-	
-	public function toResponseArray()
-	{
-		$m = $this;
-		$movie = array(
-			'id' => $m->id,
-			'name' => $m->name,
-			'description' => $m->description,
-			'director' => $m->director,
-			'actor' => $m->actor,
-			'genre' => $m->genre,
-		);
-		
-		return $movie;
-	}
+
 }
