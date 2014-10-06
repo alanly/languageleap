ecb2ccd962beda1a649c40eb17212396e9abf517
Alan Ly <me@alanly.ca>
2014-10-06 12:36:13 -0400

    Clean up code.

--- app/LangLeap/Videos/Show.php
+++ app/LangLeap/Videos/Show.php
@@ -3,29 +3,20 @@
 use Eloquent;
 use LangLeap\Payments\Billable;
 
-class Show extends Eloquent implements Billable
-{
+class Show extends Eloquent implements Billable {
 
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
 	protected $table = 'shows';
 	public $timestamps = false;		
-	
-	/**
-	* This function returns all the seasons that are associated with this show
-	*
-	*/
+
 	public function seasons()
 	{
 		return $this->hasMany('LangLeap\Videos\Season');
-	}	
+	}
+
 	public function toResponseArray()
 	{
 		$s = $this;
+
 		$show = array(
 			'id' => $s->id,
 			'name' => $s->name,
@@ -34,7 +25,7 @@ class Show extends Eloquent implements Billable
 			'director' => $s->director
 		);
 
-
 		return $show;
 	}
+
 }
