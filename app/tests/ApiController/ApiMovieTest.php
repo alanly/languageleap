a11a4a04175ab744658e26cae4849756f5d5863f
ThomasRahn <thomas@rahn.ca>
2014-10-09 18:51:35 -0400

    Tests for Movie API

--- app/tests/ApiController/ApiMovieTest.php
+++ app/tests/ApiController/ApiMovieTest.php
@@ -3,17 +3,112 @@
 use LangLeap\TestCase;
 use LangLeap\Videos\Movie;
 
+
+/**
+*
+*	@author Thomas Rahn <Thomas@rahn.ca>
+*
+*/
 class ApiMovieTest extends TestCase {
 
 	/**
-	 * Test GET all movies.
+	 * Test geting all movies.
 	 *
 	 * @return void
 	 */
-	public function testShowApi()
+	public function testIndex()
 	{
 		$response = $this->action('GET', 'ApiMovieController@index');
 		$this->assertResponseOk();
 		$this->assertJson($response->getContent());
+	}
+
+	public function testShow()
+	{
+		$this->seed();
+		$response = $this->action('GET', 'ApiMovieController@show', [1]);
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertResponseOk();
+
+		$data = $response->getData()->data;
+
+		$this->assertObjectHasAttribute('videos', $data);
+		$this->assertObjectHasAttribute('name', $data);
+
+		$this->assertEquals(1, $data->id);
+	}
+
+	public function testShowWithInvalidId()
+	{
+		$this->seed();
+		$response = $this->action('GET', 'ApiMovieController@show', [-1]);
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertResponseStatus(404);
+	}
+
+	public function testStore()
+	{
+		$response = $this->action(
+			'POST',
+			'ApiMovieController@store',
+			[],
+			['name' => 'Test']
+		);
+
+		$this->assertResponseStatus(201);
+
+		$data = $response->getData();
+
+		$this->assertEquals('success', $data->status);
+	}
+
+	public function testUpdate()
+	{
+		$this->seed();
+
+		$movie = Movie::all()->first();
+		$response = $this->action(
+			'PUT',
+			'ApiMovieController@update',
+			[$movie->id],
+			['name' => 'Test']
+		);
+
+		$this->assertResponseOk();
+		
+		$data = $response->getData();
+
+		$this->assertEquals('Test', $data->data->name);
+	}
+
+	public function testUpdateWithInvalidID()
+	{
+		$response = $this->action(
+			'PUT',
+			'ApiMovieController@update',
+			[-1],
+			['name' => 'Test']
+		);
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertResponseStatus(404);
+	}
+
+	public function testUpdateWithInvalidName()
+	{
+		$this->seed();
+
+		$movie = Movie::all()->first();
+		$response = $this->action(
+			'PUT',
+			'ApiMovieController@update',
+			[$movie->id],
+			['name' => '']
+		);
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertResponseStatus(500);
 	}
 }
