051fb743a3c12afe4bd793c66e91ea97c573c2e5
ThomasRahn <thomas@rahn.ca>
2014-10-13 10:54:56 -0400

    New player with HTML5

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -2,7 +2,7 @@
 <html lang='en'>
 	<head>
 		<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Language Leap</title>
 
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -10,30 +10,12 @@
 		<link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap/3.0.3/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//cdn.jsdelivr.net/fontawesome/4.0.3/css/font-awesome.min.css">
 		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css">	
-        <link rel="stylesheet" href="/css/style.css">
-		
-		<!--jPlayer stuff are all here-->
-		<script type="text/javascript" src="libraries/jplayer/js/jquery-2.1.1.js"></script>
-		<script type="text/javascript" src="libraries/jplayer/js/jquery.jplayer.min.js"></script>
-		<link rel="stylesheet" href="libraries/jplayer/css/style.css">
-		<script>  
-			$(document).ready(function(){  
-				$("#jquery_jplayer_1").jPlayer({  
-					ready: function () {  
-						$(this).jPlayer("setMedia", {  
-							m4v: "videos/TestVideo.mp4",  
-							ogv: "",  
-							poster: "videos/TestVideo.png"  
-						});  
-					},  
-					swfPath: "libraries/jplayer/js/Jplayer.swf",  
-					supplied: "m4v,ogv"
-				});  
-			});  
-		</script>  
-		
+	
+		@yield('javascript')
+	
 		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css">
 		<link rel="stylesheet" href="/css/admin.css">
+		@yield('css')
 	</head>
 	<body>
 		@yield('content')
--- app/views/player/player.blade.php
+++ app/views/player/player.blade.php
@@ -1,10 +1,39 @@
 @extends('master')
 
-<head>
+@section('javascript')
+<!--jPlayer stuff are all here-->
+<script type="text/javascript" src="libraries/jplayer/js/jquery-2.1.1.js"></script>
+<script type="text/javascript" src="libraries/jplayer/js/jquery.jplayer.min.js"></script>
 
-</head>
+<script>  
+	//Incase we need to add custom features
+	var video = $("#video-player");
+
+	/*$(document).ready(function(){  
+		$("#jquery_jplayer_1").jPlayer({  
+			ready: function () {  
+				$(this).jPlayer("setMedia", {  
+					m4v: "videos/TestVideo.mp4",  
+					ogv: "",  
+					poster: "videos/TestVideo.png"  
+				});  
+			},  
+			swfPath: "libraries/jplayer/js/Jplayer.swf",  
+			supplied: "m4v,ogv"
+		});  
+	});*/
+</script>  
+
+@stop
+
+@section('css')
+	<link rel="stylesheet" href="libraries/jplayer/css/style.css">
+	<link rel="stylehseet" href="css/player.css">
+@stop
+
 
 @section('content')
+<!--
 <div id="mainContainer">
 	<div id="jp_container_1" class="jp-video">
 		<div class="jp-title">
@@ -64,6 +93,46 @@
 
 			Narihibiita shoudou ga hajimari no oto ni kawaru you ni</br>
 		</p>
+	</div>
+</div>
+-->
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<!-- Player here -->
+			<div class="player-container">
+				<video width="100%"controls id="video-player">
+					<source src="videos/TestVideo.mp4" type="video/mp4">
+					Your browser does not support HTML5 video.
+				</video>
+			</div>
+
+
+		</div>
+		<div class="col-md-6">
+			<h3>Script goes here</h3>
+			<p class="script">
+				Mayowazu ni ima   mujun-darake no sekai o</br>
+				Sono te de uchihanate</br></br>
+
+				Koboreta namida no nukumori de</br>
+				Yasashisa o shitta hazu na no ni</br>
+				Doushite   mata kizutsukeatte</br>
+				Nikushimi o umidashite ikun darou</br></br>
+
+				Kishimu you na itami   shiita sono tsuyosa ga</br>
+				Itsuka mirai o yasashiku tsutsumu no darou</br></br>
+
+				Mayowazu ni ima   mujun-darake no sekai o</br>
+				Sono te de uchihanate</br>
+				Akai namida de oowareta kanashimi o</br>
+				Sotto sotto dakishimete</br></br>
+
+				Narihibiita shoudou ga hajimari no oto ni kawaru you ni</br>
+		</p>
+		</div>
 	</div>
 </div>
 
