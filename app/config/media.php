b7c8e29416f25ac1b64db74dc212f238c31fa594
Alan Ly <me@alanly.ca>
2014-10-24 18:03:08 -0400

    Add internal X-Accel paths to configuration.
    Nginx has a unique XSendfile implementation. Where as Apache and Lighttpd
    allow you to specify a path and then define your whitelist/blacklist
    separately, Nginx requires that you define a fixed location configuration
    that is only accessible to Nginx itself. This "internal location" then is
    alias'd to a specific directory where all files are served from.
    
    Therefore, for Nginx's unique case, we must maintain a list of the
    locations where videos should be served from.
    
    This commit is a part of task LANGLEAP-78, under story LAGNLEAP-37.

--- app/config/media.php
+++ app/config/media.php
@@ -1,5 +1,9 @@
 <?php 
 
+/**
+ * @author Thomas Rahn <thomas@rahn.ca>
+ * @author Alan Ly <hello@alan.ly>
+ */
 return array(
 
 	'paths' => array(
@@ -12,6 +16,30 @@ return array(
 		),
 
 		'img' => __DIR__.'/../storage/media/img',
+
+		/**
+		 * Map to the internal location stanzas in the Nginx configuration for
+		 * Nginx's XSendfile functionality (they call it X-Accel).
+		 * 
+		 * Refer to Nginx official documentation,
+		 * - http://wiki.nginx.org/X-accel
+		 * - http://wiki.nginx.org/XSendfile
+		 */
+		'xsendfile' => array(
+
+			/**
+			 * Map internal for videos. This location should alias to the parent
+			 * directory of the individual 'videos' from above.
+			 *
+			 * For example,
+			 * 	'commercials' => __DIR__.'/../storage/media/videos/commercials'
+			 *
+			 * The internal location in Nginx should alias to:
+			 * 	__DIR__.'/../storage/media/videos
+			 */
+			'videos' => '/internal/videos',
+
+		),
 	),
 
 	'test' => __DIR__.'/../storage/media/tests',
