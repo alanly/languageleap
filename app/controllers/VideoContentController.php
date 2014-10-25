e30476c6704988f51321a0c754833a753b4a3b8b
Alan Ly <me@alanly.ca>
2014-10-25 01:17:26 -0400

    Add controller for video clips.
    THe serving of video clips to the client must be handled via a controlled
    method that allows for authentication and authorization capabilities. The
    addition of the controller in this commit allows for the fulfillment of
    this requirement by allowing static video content to be served in a
    controlled manner.
    
    The commit contains the controller, the associated tests, and an
    abstraction class. The abstraction class basically acts as a factory for
    the SplFileInfo class. This allows us to swap in a mock class for the
    purposes of testing.
    
    This commit is a part of task LANGLEA-78 and is associated with story
    LANGLEAP-37.

---
+++ app/controllers/VideoContentController.php
@@ -0,0 +1,73 @@
+<?php
+
+use LangLeap\Core\FileInfoFactory;
+use LangLeap\Videos\Video;
+
+/**
+ * @author  Alan Ly <hello@alan.ly>
+ */
+class VideoContentController extends \BaseController {
+
+	protected $videos;
+	protected $fileInfoFactory;
+
+
+	public function __construct(Video $videos, FileInfoFactory $fileInfoFactory)
+	{
+		// Get reference for the database repository instance.
+		$this->videos = $videos;
+
+		// Get a reference to the FileInfo factory.
+		$this->fileInfoFactory = $fileInfoFactory;
+	}
+
+
+	/**
+	 * Stream the video resource.
+	 * 
+	 * @param  int $id
+	 * @return Symfony\Component\HttpFoundation\BinaryFileResponse
+	 */
+	public function getVideo($id)
+	{
+		$video = $this->videos->find($id);
+
+		if (! $video)
+		{
+			return $this->apiResponse('error', "Video {$id} not found.", 404);
+		}
+
+		// Get an instance of SplFileInfo from the factory.
+		$file = $this->fileInfoFactory->makeInstance($video->path);
+
+		if (! ($file->isFile() && $file->isReadable()))
+		{
+			return $this->apiResponse('error', "An error occurred while reading video {$id}.", 500);
+		}
+
+		// Create the Sendfile headers for this file.
+		$headers = $this->getSendfileHeadersForFile($file);
+
+		return Response::download($file, null, $headers);
+	}
+
+
+	/**
+	 * Based on the given file, this returns an array containing the appropriate
+	 * XSendfile header-fields for Apache, Lighttpd, and Nginx.
+	 * 
+	 * @param  SplFileInfo $file
+	 * @return array
+	 */
+	protected function getSendfileHeadersForFile(SplFileInfo $file)
+	{
+		$parentDirName = dirname($file->getRealPath()).'/';
+
+		return [
+			'X-Sendfile'           => $file->getRealPath(), // Sendfile path for Apache
+			'X-LIGHTTPD-send-file' => $file->getRealPath(), // Sendfile path for Lighttpd
+			'X-Accel-Redirect'     => Config::get('media.paths.xsendfile.videos').$parentDirName.$file->getFilename(),
+		];
+	}
+
+}
