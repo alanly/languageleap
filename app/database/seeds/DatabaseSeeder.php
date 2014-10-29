7a2b0fb78de77dc2a9a0a8308f994d01fb375a60
ThomasRahn <thomas@rahn.ca>
2014-10-29 18:33:26 -0400

    Fixed DB seeder order

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 		
 		$this->call('CommercialTableSeeder');
-		$this->call('DefinitionTableSeeder');
+		$this->call('DefinitionTableSeeder');	
+		$this->call('ShowTableSeeder');
+		$this->call('SeasonTableSeeder');
 		$this->call('EpisodeTableSeeder');
 		$this->call('MovieTableSeeder');
 		$this->call('ScriptTableSeeder');
-		$this->call('SeasonTableSeeder');
-		$this->call('ShowTableSeeder');
 		$this->call('WordTableSeeder');
 		$this->call('VideoTableSeeder');
 	}
