0b830c97612d82db36ffbd49e6f6d0af5d9b72fb
David Siekut <davidsiekut@gmail.com>
2014-11-05 15:19:16 -0500

    Revert admin master.php from John's merge which removed the proper layout.

--- app/views/admin/master.blade.php
+++ app/views/admin/master.blade.php
@@ -2,17 +2,15 @@
 <html lang='en'>
 	<head>
 		<meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Language Leap</title>
 
-        <title>Language Leap</title>
-
-		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="//cdn.jsdelivr.net/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap/3.0.3/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//cdn.jsdelivr.net/fontawesome/4.0.3/css/font-awesome.min.css">
-		@yield('head')
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css">
+		<link rel="stylesheet" href="/css/admin.css">
 	</head>
 	<body>
 		@yield('content')
