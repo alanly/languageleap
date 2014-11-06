5bf395e37a947b0d9fb1eccc0db5cf29f2f38330
Alan Ly <me@alanly.ca>
2014-11-06 13:30:49 -0500

    Harmonize ext. imports for client/admin master.
    The existing issue was that the main libraries being used were all over
    the place and of varying versions. This commit brings the the common
    portion of the master views together under one file, and ensures that
    we're using the latest versions of the Bootstrap and jQuery libraries.

---
+++ app/views/layouts/base.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>Language Leap</title>
+
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
+
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.0/js/bootstrap.min.js"></script>
+		@yield('javascript')
+
+		@yield('head')
+	</head>
+
+	<body>
+		@yield('content')
+	</body>
+</html>
