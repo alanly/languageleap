7fedac06782b75d7e1567c056dbaf648d8807bdd
ThomasRahn <thomas@rahn.ca>
2014-11-06 17:45:33 -0500

    LANGLEAP-72: Initial development for question submissions

--- app/controllers/ApiQuizController.php
+++ app/controllers/ApiQuizController.php
@@ -29,11 +29,18 @@ class ApiQuizController extends \BaseController {
 			);
 		}
 
-		//Create quiz instance
-		$quiz = new Quiz;
-		$quiz->user_id = 1;
-		$quiz->video_id = $video_id;
-		$quiz->save();
+		//Check if there is a quiz associated to this video and user id
+		//TODO: NEED TO CHECK USER ID (before authentication was done)
+		$quiz = Quiz::where("video_id",$video_id);
+
+		if(! $quiz){
+			//Create quiz instance
+			$quiz = new Quiz;
+			$quiz->user_id = 1;
+			$quiz->video_id = $video_id;
+			$quiz->save();
+		}
+
 
 		//All the selected words. This list can be empty.
 		$selected_words = Input::get("selected_words");
@@ -93,6 +100,8 @@ class ApiQuizController extends \BaseController {
 			array_push($questions, $question);
 		}
 
+		//TODO: if there is not enough questions make some from the all_words array
+
 		$jsonResponse = $this->generateJsonResponse($quiz, $questions[0], $definitions);
 
 		return $this->apiResponse(
@@ -101,18 +110,84 @@ class ApiQuizController extends \BaseController {
 		);
 	}
 
+
+	public function update($id)
+	{
+		$question = Question::find($id);
+
+		if (! $question)
+		{
+			return $this->apiResponse(
+				'error',
+				"question {$id} not found.",
+				404
+			);
+		}
+
+		$selected_id = Input::get("selected_id");
+
+		if(! $selected_id)
+		{
+			return $this->apiResponse(
+				'error',
+				"Selected id: {$id} is invalid",
+				400
+			);
+		}
+
+		$question->selected_id = $selected_id;
+		$question->save();
+
+		//get the quiz
+		$quiz = $question->quiz()->first();
+
+		if( $selected_id == $question->definition_id)
+		{
+			//Increase the score because they selected the correct definition
+			$quiz->increment("score");
+		}
+		
+		$newQuestion = $quiz->questions()->where("selected_id", null)->first();
+
+		if( $newQuestion)
+		{
+			return $this->apiResponse(
+				'success',
+				array("Score" => $quiz->score),
+				200
+			);	
+		}
+		return $this->apiResponse(
+			'success',
+			$newQuestion->toArray(),
+			200
+		);	
+	}
+
 	/**
 	*	This function will generate the valid json response for a valid quiz
 	*
 	*/
 	protected function generateJsonResponse(Quiz $quiz, Question $question, $definitions)
 	{
-
 		return array(
 			"quiz" => $quiz->id,
-			"question" => $question->toArray(),
-			"definitions" => $definitions,
+			"question" => $question->question,
+			"question_id" => $question->id,
+			"definitions" => $this->definitionResponse($definitions),
 		);
+	}
+
+	/**
+	*	This function will return an array of all the "definition" attributes of all the defintions.
+	*/
+	protected function definitionResponse($definitions){
+		$jsonDefinition = array();
+
+		foreach($definitions as $def)
+			array_push($jsonDefinition, $def->definition);
+
+		return $jsonDefinition;
 	}
 }
 
