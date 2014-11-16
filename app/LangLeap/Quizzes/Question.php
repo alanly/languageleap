e9cf670d2012443a812bbc6bc80509aa8a1a7cda
Alan Ly <me@alanly.ca>
2014-11-16 00:11:42 -0500

    Clean up spacing; add "unanswered" query scope.
    Added a scope to filter through unanswered questions.

--- app/LangLeap/Quizzes/Question.php
+++ app/LangLeap/Quizzes/Question.php
@@ -1,28 +1,27 @@
 <?php namespace LangLeap\Quizzes;
 
 use Eloquent;
-class Question extends Eloquent
-{
+
+class Question extends Eloquent {
+
+	public    $timestamps = false;
 
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'questions';
-	public $timestamps = false;
-	/**
-	*	This function returns the quiz that contains this question
-	*
-	*/
 	public function quiz()
 	{
 		return $this->belongsTo('LangLeap\Quizzes\Quiz');
 	}
 
+
 	public function definition()
 	{
 		return $this->belongsTo('LangLeap\Words\Definition');
 	}
+
+
+	public function scopeUnanswered($query)
+	{
+		return $query->where('selected_id', null);
+	}
+
 }
