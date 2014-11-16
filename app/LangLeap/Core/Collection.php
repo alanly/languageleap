9b7749c98a4864886335f0f5023676bc376eece4
Alan Ly <me@alanly.ca>
2014-11-16 02:25:44 -0500

    Extend Eloquent\Collection class.
    Existing `Illuminate\Database\Eloquent\Collection` class did not provide
    the appropriate behaviour for certain methods where you would expect it to
    operate over a primary key (instead, operating on the internal array
    index).
    
    LANGLEAP-72

---
+++ app/LangLeap/Core/Collection.php
@@ -0,0 +1,73 @@
+<?php namespace LangLeap\Core;
+
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @author Alan Ly <hello@alan.ly>
+ */
+class Collection extends EloquentCollection {
+
+	/**
+	 * Remove a model from the collection by key.
+	 *
+	 * @param  mixed  $key
+	 * @return void
+	 */
+	public function forget($key)
+	{
+		$this->pull($key);
+	}
+
+
+	/**
+	 * Pulls a model from the collection.
+	 * 
+	 * @param  mixed  $key
+	 * @param  mixed  $default
+	 * @return mixed
+	 */
+	public function pull($key, $default = null)
+	{
+		if ($key instanceof Model)
+		{
+			$key = $key->getKey();
+		}
+
+		foreach ($this->items as $index => $model)
+		{
+			if ($model->getKey() == $key)
+			{
+				$needle = $model;
+				unset($this->items[$index]);
+				return $needle;
+			}
+		}
+
+		return $default;
+	}
+
+
+	/**
+	 * Pull one or more models randomly from the collection.
+	 *
+	 * @param  int  $amount
+	 * @return mixed
+	 */
+	public function pullRandom($amount = 1)
+	{
+		if ($this->isEmpty()) return null;
+
+		$keys = array_rand($this->items, $amount);
+	
+		$items = is_array($keys) ? array_intersect_key($this->items, array_flip($keys)) : $this->items[$keys];
+
+		foreach ($keys as $key)
+		{
+			unset($this->items[$key]);
+		}
+
+		return $items;
+	}
+	
+}
