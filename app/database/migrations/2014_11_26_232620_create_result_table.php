28b62272bf2dae0ffe0f479aed5c390aaef4dc7a
Dror Ozgaon <dror.ozgaon@gmail.com>
2014-11-26 18:31:26 -0500

    LANGLEAP-98 : Created Results table.

---
+++ app/database/migrations/2014_11_26_232620_create_result_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('results', function($table){
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('videoquestion_id')->unsigned();
+			$table->boolean('is_correct');
+			$table->dateTime('timestamp');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('results');
+	}
+
+}
