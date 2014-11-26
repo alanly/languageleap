ccd9503e8ea6451ae147d3751a87d33e299abadc
Dror Ozgaon <dror.ozgaon@gmail.com>
2014-11-26 18:57:25 -0500

    LANGLEAP-98: Created the Eloquent model for VideoQuestion association.

---
+++ app/LangLeap/Quizzes/VideoQuestion.php
@@ -0,0 +1,27 @@
+<?php namespace LangLeap\Quizzes;
+
+use Eloquent;
+/**
+ * @author  Dror Ozgaon <Dror.Ozgaon@gmail.com>
+ */
+class VideoQuestion extends Eloquent {
+
+	public    $timestamps = false;
+	protected $fillable   = ['video_id', 'question_id', 'is_custom'];
+	protected $rules      = [
+		'video_id'   			=> 'required|integer',
+		'question_id'    		=> 'required|integer',
+		'is_custom'				=> 'required|boolean'
+		];
+
+
+	public function video()
+	{
+		return $this->belongsTo('LangLeap\Videos\Video');
+	}
+
+	public function question()
+	{
+		return $this->belongsTo('LangLeap\Quizzes\Question');
+	}
+}
