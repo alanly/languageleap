275747d64a9342e0dcd83094c2111ce37bfa712a
Dror Ozgaon <dror.ozgaon@gmail.com>
2014-11-26 18:24:47 -0500

    LANGLEAP-98 :  Created Answer table.

---
+++ app/database/migrations/2014_11_26_232209_create_answer_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswerTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('answers', function($table){
+			$table->increments('id');
+			$table->integer('question_id')->unsigned();
+			$table->string('answer');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('answers');
+	}
+
+}
