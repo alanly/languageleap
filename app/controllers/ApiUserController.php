e09fd32fb4613de21941015f26c4dfd7346b78f7
Michael Lavoie <lavoie6453@gmail.com>
2014-11-26 20:06:28 -0500

    LANGLEAP-84: Implemented the User API.

--- app/controllers/ApiUserController.php
+++ app/controllers/ApiUserController.php
@@ -4,14 +4,54 @@ use LangLeap\Accounts\User;
 
 /**
  * @author Thomas Rahn <thomas@rahn.ca>
+ * @author Michael Lavoie <lavoie6453@gmail.com>
  */
 class ApiUserController extends \BaseController {
 
 	protected $users;
 
-	public function __construct(User $user)
+	public function __construct(User $users)
 	{
-		$this->userss = $user;
+		$this->users = $users;
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$users = User::all();
+	
+		return $this->apiResponse("success", $users->toArray());
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		$user = new User;
+
+		$user->fill(Input::get());
+
+		if (! $user->save())
+		{
+			return $this->apiResponse(
+				'error',
+				$user->getErrors(),
+				500
+			);
+		}
+
+		return $this->apiResponse(
+			'success',
+			$user->toArray(),
+			201
+		);	
 	}
 
 	/**
@@ -20,9 +60,85 @@ class ApiUserController extends \BaseController {
 	 * @param  int  $userId
 	 * @return JsonResponse
 	 */
-	public function show($id)
+	public function show($userId)
 	{
-	
+		$users = User::find($userId);
+		
+		if (!$users)
+		{
+			return $this->apiResponse(
+				'error',
+				"User {$userId} not found.",
+				404
+			);
+		}
+		
+		return $this->apiResponse("success", $users->toArray());
 	}
 
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $userId
+	 * @return Response
+	 */
+	public function update($userId)
+	{
+		$user = User::find($userId);
+
+		if (! $user)
+		{
+			return $this->apiResponse(
+				'error',
+				"User {$userId} not found.",
+				404
+			);
+		}
+
+		$user->fill(Input::get());
+
+		if (! $user->save())
+		{
+			return $this->apiResponse(
+				'error',
+				$user->getErrors(),
+				500
+			);
+		}
+
+		return $this->apiResponse(
+			'success',
+			$user->toArray(),
+			200
+		);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $userId
+	 * @return Response
+	 */
+	public function destroy($userId)
+	{
+		$user = User::find($userId);
+
+		if (! $user)
+		{
+			return $this->apiResponse(
+				'error',
+				"User {$userId} not found.",
+				404
+			);
+		}
+
+		$user->seasons()->delete();
+		$user->delete();
+
+		return $this->apiResponse(
+			'success',
+			'User {$userId} has been removed',
+			200
+		);
+	}
 }
