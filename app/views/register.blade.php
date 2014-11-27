443012ca66a411b0c7a2cadfe3e6dfe68b9f24bc
kc-wan <martinwan1992@hotmail.com>
2014-11-26 21:48:56 -0500

    changed html to laravel convention

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -1,4 +1,5 @@
 @extends('master')
+
 @section('css')
 	<style>
 		.form-control
@@ -7,68 +8,62 @@
 		}
 	</style>
 @stop
+
+@section('javascript')
+@stop
+
 @section('content')
 	<form class="form-horizontal" role="form">
 		<div class="form-group">
 			<h2 id="forms-horizontal" class="col-sm-3	control-label">Account Creation</h2>
 		</div>
 		<div class="form-group">
-			<label for="inputUsername" class="col-sm-2 control-label">Username</label>
+			{{Form::label('usernameLabel', 'Username', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputUsername" placeholder="example1234">
+				{{Form::text('username', '' , array('class' => 'form-control', 'placeholder' => 'example1234', 'id' => 'usernameInput'))}}
 				<p class="form-control-static">You will use this to login</p>
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="inputPassword" class="col-sm-2 control-label">Password</label>
+			{{Form::label('password', 'Password', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
-				<input type="password" class="form-control" id="inputPassword" placeholder="Password">
+				{{Form::password('password', array('class' => 'form-control', 'placeholder' => 'Enter password', 'id' => 'passwordInput'))}}
 				<p class="form-control-static">Minimum 6 to 12 characters maximum.</p>
 			</div>
-			<label for="inputPassword" class="col-sm-2 control-label">Re-enter Password</label>
+			{{Form::label('confPassword', 'Confirm Password', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
-				<input type="password" class="form-control" id="confPassword" placeholder="Confirm Password">
+				{{Form::password('confpassword', array('class' => 'form-control', 'placeholder' => 'Re-enter password', 'id' => 'confpasswordInput'))}}
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="inputEmail" class="col-sm-2 control-label">Email</label>
+			{{Form::label('email', 'E-mail', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
-				<input type="email" class="form-control" id="inputEmail" placeholder="email@example.com">
+				{{Form::text('email', '', array('class' => 'form-control', 'placeholder' => 'email@example.com', 'id' => 'emailInput'))}}
 				<p class="form-control-static">You will receive emails regarding taking quizzes</p>
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="genderInput" class="col-sm-2 control-label">Gender</label>
+			{{Form::label('gender', 'Gender', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
-				<div class="radio">
-					<label>
-						<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
-						Male
-					</label></br>
-					<label>
-						<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-						Female
-					</label>
-				</div>
+					{{Form::radio('gender', 'male', false, array('id' => 'male'))}}
+					{{Form::label('male', 'Male')}}</br>
+					{{Form::radio('gender', 'female', false, array('id' => 'female'))}}
+					{{Form::label('male', 'Female')}}
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="termsOfService" class="col-sm-2 control-label">Terms of Service</label>
+			{{Form::label('termsOfService', 'Terms of Service', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
-				<div class="checkbox">
-					<label>
-						<input type="checkbox" name="tos" id="tos1">
-						I agree that this account belongs to me and me only. I understand that account sharing is prohibited.
-					</label>
-				</div>
+				{{Form::checkbox('agreement', 'termsOfService', false, array('id' => 'terms'))}}
+				{{Form::label('tos', 'I agree that this account belongs to me and me only. I understand that account sharing is prohibited.', array('class' => 'text'))}}
 			</div>
 		</div>
 		<div class="form-group">
 			<div class="col-sm-3 control-label">
-				<button type="submit" class="btn btn-default">Register</button>
+				{{Form::submit('Register', array('class' => 'btn btn-default'))}}
 			</div>
 			<div class="col-sm-1 control-label">
-				<button type="reset" class="btn btn-default">Reset Fields</button>
+				{{Form::reset('Reset fields', array('class' => 'btn btn-default'))}}
 			</div>
 		</div>
 	</form>			
