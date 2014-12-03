dad3dd61aa874363fee80932dc5778e53423a643
Michael Lavoie <lavoie6453@gmail.com>
2014-12-02 20:36:16 -0500

    Fixed the tests for the ApiUserController.

--- app/tests/ApiController/ApiUserTest.php
+++ app/tests/ApiController/ApiUserTest.php
@@ -9,58 +9,100 @@ use LangLeap\Accounts\User;
  */
 class ApiUserControllerTest extends TestCase {
 
-	public function testIndex()
-	{
-		// Seed data for testing
-		$this->seed();
-
-		// Get the user with id = 1
-		$response = $this->action(
-			'GET',
-			'ApiUserController@index'
-		);
-
-		$this->assertResponseOk();
-	}
-
 	public function testStore()
 	{
+		$userData = [
+			'username' => 'testUser123',
+			'email'=>'test@tester.com',
+			'first_name' => 'John',
+			'last_name'=>'Doe',
+			'password' => 'password123'
+		];
+
+		// Create a new user
 		$response = $this->action(
 			'POST',
 			'ApiUserController@store',
 			[],
-			['username' => 'testUser123',
-			'email'=>'test@tester.com',
-			'first_name' => 'John',
-			'last_name'=>'Doe',
-			'password' => 'password123']
+			$userData
 		);
 
-		// Test for success response
+		// Test for successful creation of user response
 		$this->assertResponseStatus(201);
+
+		// Attempt to create a new user that already exists
+		// Note: This should be sufficient to partially test the registration validation.
+		$response = $this->action(
+			'POST',
+			'ApiUserController@store',
+			[],
+			$userData
+		);
+
+		// Test for the unsuccessful creation of a user
+		$this->assertResponseStatus(400);
+
+		$user = new User($userData);
+
+		// Set the currently authenticated user
+		$this->be($user);
+
+		// Attempt to create a new user while already being logged in
+		$response = $this->action(
+			'POST',
+			'ApiUserController@store',
+			[],
+			$userData
+		);
+
+		// Test for the unsuccessful creation of a user
+		$this->assertResponseStatus(401);
 	}
 
 	public function testShow()
 	{
 		$this->seed();
 
-		$response = $this->action('GET', 'ApiUserController@show', 1);
+		$user = User::first();
+		
+		// Set the currently authenticated user
+		$this->be($user);
+
+		$response = $this->action('GET', 'ApiUserController@show', $user->id);
 
 		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
 		$this->assertResponseOk();
+
+		// Try to get a user that isn't the currently authenticated user
+		$response = $this->action('GET', 'ApiUserController@show', $user->id + 1);
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertResponseStatus(401);
+
 	}
 
 	public function testUpdate()
 	{
 		$this->seed();
 
-		// Update a couple of fields of user with id = 1
+		$user = User::first();
+		
+		// Set the currently authenticated user
+		$this->be($user);
+		$user->first_name = 'Johnny';
+		$user->last_name = 'Depp';
+
+		$userDataArray = $user->toArray();
+		$userDataArray["password"] = "testerpassword";
+
+		//unset($userDataArray['id']);
+
+		// Update a couple of fields of the user
 		$response = $this->action(
 			'PATCH',
 			'ApiUserController@update',
-			1,
-			['first_name' => 'Johnny',
-			'last_name' => 'Depp']
+			$user->id,
+			$userDataArray
 		);
 
 		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
@@ -71,11 +113,11 @@ class ApiUserControllerTest extends TestCase {
 		$this->assertEquals('Johnny', $data->first_name);
 		$this->assertEquals('Depp', $data->last_name);
 
-		// Get user with id = 1
+		// Get user
 		$response = $this->action(
 			'GET',
 			'ApiUserController@show',
-			1
+			$user->id
 		);
 
 		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
@@ -91,22 +133,29 @@ class ApiUserControllerTest extends TestCase {
 	{
 		$this->seed();
 
-		// Delete user with id = 1
+		$user = User::first();
+		
+		// Set the currently authenticated user
+		$this->be($user);
+
+		// Delete user
 		$response = $this->action(
 			'DELETE',
 			'ApiUserController@destroy',
-			1
+			$user->id
 		);
 
+		// Verify successful deletion
 		$this->assertResponseStatus(204);
 
 		// Get user with id = 1
 		$response = $this->action(
 			'GET',
 			'ApiUserController@show',
-			1
+			$user->id
 		);
 
+		// User should
 		$this->assertResponseStatus(404);
 	}
 	
