edfa18fd37a5821ea7de027ac7e1300b280a3986
kc-wan <martinwan1992@hotmail.com>
2014-12-05 00:01:10 -0500

    did simple validation for now. need to figure out how to jquery

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -10,26 +10,69 @@
 @stop
 
 @section('javascript')
+	<!--<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.js">	</script>
+	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>-->
+	<script>
+		/*$(document).ready(function() {
+			$("#registrationForm").validate({
+				rules: {
+					field: {
+						username: {
+							required: true,
+							minlength: 3,
+							maxlength: 12
+						},
+						password: {
+							required: true,
+							minlength: 6,
+							maxlength: 21
+						},
+						email: {
+							required: true,
+							email: true
+						}
+					}
+				}
+			});
+		});*/
+		function validateForm() {
+			var username = document.forms["regForm"]["username"].value;
+			if(username == null || username == "")
+			{
+				alert("Please enter a valid username");
+				return false;
+			}
+		
+			var useremail = document.forms["regForm"]["email"].value;
+			var atpos = useremail.indexOf("@");
+			var dotpos = useremail.lastIndexOf(".");
+			if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=useremail.length)
+			{
+				alert("Invalid email address");
+				return false;
+			}
+		}
+	</script>
 @stop
 
 @section('content')
-	<form class="form-horizontal" role="form">
+	<form id="registrationForm" name="regForm" class="form-horizontal" role="form" onsubmit="return validateForm()">
 		<div class="form-group">
 			<h2 id="forms-horizontal" class="col-sm-3	control-label">Account Creation</h2>
 		</div>
 		<div class="form-group">
 			{{Form::label('usernameLabel', 'Username', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
-				{{Form::text('username', '' , array('class' => 'form-control', 'placeholder' => 'example1234', 'id' => 'usernameInput'))}}
-				<p class="form-control-static">You will use this to login</p>
+				{{Form::text('username', '', array('class' => 'form-control', 'placeholder' => 'example1234', 'id' => 'usernameInput'))}}
 			</div>
 		</div>
 		<div class="form-group">
 			{{Form::label('password', 'Password', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
 				{{Form::password('password', array('class' => 'form-control', 'placeholder' => 'Enter password', 'id' => 'passwordInput'))}}
-				<p class="form-control-static">Minimum 6 to 12 characters maximum.</p>
 			</div>
+		</div>
+		<div class="form-group">
 			{{Form::label('confPassword', 'Confirm Password', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
 				{{Form::password('confpassword', array('class' => 'form-control', 'placeholder' => 'Re-enter password', 'id' => 'confpasswordInput'))}}
@@ -39,7 +82,6 @@
 			{{Form::label('email', 'E-mail', array('class' => 'col-sm-2 control-label'))}}
 			<div class="col-sm-10">
 				{{Form::text('email', '', array('class' => 'form-control', 'placeholder' => 'email@example.com', 'id' => 'emailInput'))}}
-				<p class="form-control-static">You will receive emails regarding taking quizzes</p>
 			</div>
 		</div>
 		<div class="form-group">
