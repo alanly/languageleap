d161c58b6d498be1e606898fad5a9347d00ddffa
John Di Girolamo <jdigirolamo@hotmail.com>
2014-12-05 11:00:22 -0500

    Modified the side nav content appropriately.

--- app/views/admin/landing.blade.php
+++ app/views/admin/landing.blade.php
@@ -33,16 +33,9 @@
 	    <div class="col-md-2 padded">
 	          <ul class="nav nav-pills nav-stacked">
 	            <li class="active"><a href="#">Home</a></li>
-	            <li class="dropdown">
-	              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu 1 <span class="caret"></span></a>
-	              <ul class="dropdown-menu">
-	                <li><a href="#">Submenu 1-1</a></li>
-	                <li><a href="#">Submenu 1-2</a></li>
-	                <li><a href="#">Submenu 1-3</a></li>                        
-	              </ul>
-	            </li>
-	            <li><a href="#">Menu 2</a></li>
-	            <li><a href="#">Menu 3</a></li>
+	            <li><a href="#">Upload Videos</a></li>
+	            <li><a href="#">Upload Scripts</a></li>
+	            <li><a href="#">Insert Definitions</a></li>
 	        </ul>
 	    </div>
 
