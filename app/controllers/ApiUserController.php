4dc53a383799ef43a26e9432badeb029f25e01e7
Michael Lavoie <lavoie6453@gmail.com>
2014-12-08 16:21:15 -0500

    Added a function to verify a user with the provided confirmation code.

--- app/controllers/ApiUserController.php
+++ app/controllers/ApiUserController.php
@@ -173,4 +173,26 @@ class ApiUserController extends \BaseController {
 		}
 	}
 
+	/**
+	* Attempt to confirm the user with the provided confirmation code.
+	*/
+	public function confirm($confirmation_code)
+	{
+		if(! $confirmation_code)
+		{
+			throw new InvalidConfirmationCodeException;
+		}
+
+		$user = User::whereConfirmationCode($confirmation_code)->first();
+
+		if (! $user)
+		{
+			throw new InvalidConfirmationCodeException;
+		}
+
+		$user->is_confirmed = 1;
+		$user->confirmation_code = null;
+		$user->save();
+	}
+
 }
