6f54d9debc39c3032514390de661c8b9d4aa00ec
Alan Ly <me@alanly.ca>
2014-12-07 21:19:49 -0500

    Minor fixes to login view.
    Instead of defining a static URL, we're using the "action" property to
    refer to the correct controller method. Makes it easier to deal with any
    potential routing changes down the line.
    
    Cleaned up the formatting of the code a bit. Removed a trailing
    parenthesis set.
    
    LANGLEAP-90

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -2,14 +2,14 @@
 
 @section('css')
 <style type="text/css">
-	.login-form{
+	.login-form {
 		min-width: 300px;
 		padding: 15px 30px 15px 30px;
 		margin: 15% auto;
 		width: 30%;
 		border: solid 1px #d4d4d4;
 	}
-	.brand{
+	.brand {
 		font-weight: 800 !important;
 		padding: 10px 10px 10px 10px;
 		text-align: center;
@@ -18,42 +18,44 @@
 @stop
 
 @section('content')
-	<div class="container login-form">
-		<h3 class="brand">LanguageLeap</h3>
-		@if(Session::has('action.failed') === true)
-			<div class="alert {{ Session::get('action.failed') === false ? 'alert-info' : 'alert-danger' }}">
-				<p>{{ Session::get('action.message') }}</p>
-			</div>
-		@endif
-		<div class="row">
-			{{ Form::open(array('url' => '/auth/login', 'class' => 'form-horizontal', 'placeholder' => 'form')) }}
-				<div class="form-group">
-					{{ Form::label('Username','', array('class' => 'col-sm-2 control-label')) }}
-					<div class="col-sm-10">
-						{{ Form::text('username','',array('class' => 'form-control', 'placeholder' => 'Username')) }}
-					</div>
-				</div>
-				<div class="form-group">
-					{{ Form::label('Password','', array('class' => 'col-sm-2 control-label')) }}
-					<div class="col-sm-10">
-						{{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-						<div class="checkbox">
-							<label>
-								{{ Form::checkbox('remember') }} Remember me
-							</label>
-						</div>
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-						{{ Form::submit('Sign in', array('class' => 'btn btn-primary')) }}
-					</div>
-				</div>
-			{{ Form::close() }}
+<div class="container login-form">
+	<h3 class="brand">LanguageLeap</h3>
+
+	@if(Session::has('action.failed') === true)
+		<div class="alert {{ Session::get('action.failed') === false ? 'alert-info' : 'alert-danger' }}">
+			<p>{{ Session::get('action.message') }}</p>
 		</div>
+	@endif
+	
+	<div class="row">
+		{{ Form::open(array('action' => 'AuthController@postLogin', 'class' => 'form-horizontal', 'placeholder' => 'form')) }}
+			<div class="form-group">
+				{{ Form::label('Username','', array('class' => 'col-sm-2 control-label')) }}
+				<div class="col-sm-10">
+					{{ Form::text('username','',array('class' => 'form-control', 'placeholder' => 'Username')) }}
+				</div>
+			</div>
+			<div class="form-group">
+				{{ Form::label('Password','', array('class' => 'col-sm-2 control-label')) }}
+				<div class="col-sm-10">
+					{{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<div class="checkbox">
+						<label>
+							{{ Form::checkbox('remember') }} Remember me
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					{{ Form::submit('Sign in', array('class' => 'btn btn-primary')) }}
+				</div>
+			</div>
+		{{ Form::close() }}
 	</div>
-@stop()
+</div>
+@stop
