2cd16ff1bfe7baafd326682b3ace2889f3606e80
Alan Ly <me@alanly.ca>
2014-12-07 20:19:11 -0500

    Clean up and refactor code.
    Cleaned up the code for the `User` model; added annotation for authorship.
    
    Refactored AuthController, simplifying if-logic paths. Modified behaviour
    of the `getLogout` method because the action shouldn't result in a JSON
    response in accordance with the overarching behaviour of the other
    methods and with its intended purpose. It's suppose to redirect the user
    to a particular page. Unless we're making an API for a front-end client
    here, in which case the other methods are incorrect.
    
    LANGLEAP-90

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -3,59 +3,63 @@
 use LangLeap\Accounts\User;
 
 /**
-* @author Thomas Rahn <thomas@rahn.ca>
-*/
-class AuthController extends BaseController{
+ * @author Thomas Rahn <thomas@rahn.ca>
+ * @author Alan Ly <hello@alan.ly>
+ */
+class AuthController extends BaseController {
 
-	/**
-	 *	Return the login page
-	 */
 	public function getLogin()
 	{
+		if (Auth::check()) return $this->authRedirect();
+
 		return View::make('auth.login');
 	}
 
-	/**
-	 *	This function will attempt to login with a username and password. 
-	 *	If the username password combination is successfull it will redirect to the index page. 
-	 *	Otherwise returns back the same page with the appropriate error message
-	 */
+
 	public function postLogin()
 	{
-		$success = Auth::attempt(array('username' => Input::get('username'), 'password' => Input::get('password')), Input::has('remember'));
+		$success = Auth::attempt(
+			[
+				'username' => Input::get('username'),
+				'password' => Input::get('password')
+			],
+			Input::has('remember')
+		);
 
-		if($success)
+		if (! $success)
 		{
-			//Successful login, verification if user is an administrator
-			if(Auth::user()->is_admin)
-			{
-				return Redirect::intended('/admin');
-			}
-			else 
-			{
-				return Redirect::intended('/');
-			}
+			// Redirect to login page with appropriate error messages.
+			return Redirect::route('login')
+				->with("action.failed", true)
+				->with("action.message", "Invalid username or password");
 		}
-		else
-		{
-			//Login was unsuccessful, return to login page with appropriate error messages.
-			return Redirect::route('login')->with("action.failed", true)->with("action.message", "Invalid username or password");
-		}
+
+		return $this->authRedirect();
 	}
 
-	/**
-	 *	Attempt to logout.
-	 */
+
 	public function getLogout()
 	{
 		Auth::logout();
 
-		$success = ! Auth::check();
+		$failed = Auth::check();
 
-		return $this->apiResponse(
-			($success) ? "success" : "error",
-			($success === false) ? "Logout has failed" : "Successfully logged out"
-		);
+		if ($failed)
+		{
+			return Redirect::route('login')
+				->with('action.failed', true)
+				->with('action.message', 'Unable to logout. Please contact an administrator.');
+		}
 
+		return Redirect::route('login')
+			->with('action.failed', false)
+			->with('action.message', 'You have been logged out.');
 	}
+
+
+	protected function authRedirect()
+	{
+		return Redirect::intended((Auth::user()->is_admin) ? '/admin' : '/');
+	}
+
 }
