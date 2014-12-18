a7d9e547a0f102b425203ad01227bb8a4c14b427
ThomasRahn <thomas@rahn.ca>
2014-12-18 18:26:54 -0500

    LANGLEAP-108: added language model

---
+++ app/LangLeap/Core/Language.php
@@ -0,0 +1,10 @@
+<?php namespace LangLeap\Core;
+
+/**
+ * @author Thomas Rahn <thomas@rahn.ca>
+ * @author Alan Ly <hello@alan.ly>
+ */
+class Language extends ValidatedModel {
+{
+	public $timestamps = false;
+}
