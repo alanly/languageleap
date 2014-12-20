dc470e99c9d3087d7634bad40c66eb546d4f9fc5
ThomasRahn <thomas@rahn.ca>
2014-12-20 11:17:54 -0500

    LANGLEAP-107: added language page for admin main page

---
+++ app/lang/fr/admin.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+	
+	/*
+	|------------------------------------------------------------------
+	| Player Language Lines.
+	|------------------------------------------------------------------
+	|
+	| The following language lines will be used for the english 
+	| version of the player page.
+	|
+	*/
+
+	'media' => array(
+		'movie' => array(
+			'name' => 'Movies',
+			'table' => array(
+				'id' => 'Id',
+ 			),
+		),
+		'commercial' => array(
+			'name' => 'Commercials',
+			'table' => array(
+			),
+		),
+		'show' => array(
+			'name' => 'Tv Shows',
+			'table' => array(
+			),
+		),
+	),
+);
