8bfc13f22159cec1a157b28f01ecd3de50be47be
Dror Ozgaon <dror.ozgaon@gmail.com>
2014-12-30 10:19:06 -0500

    LANGLEAP-125 : Created the Levels table seed.

---
+++ app/database/seeds/LevelTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+class LanguageTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('levels')->delete();
+
+		$unranked = App::make('LangLeap\Levels\Level');
+		$unranked = $unranked->create(['code' => 'ur', 'description'=>'Unranked']);
+		
+		$beginner = App::make('LangLeap\Levels\Level');
+		$beginner = $beginner->create(['code' => 'be', 'description'=>'Beginner']);
+
+		$intermediate = App::make('LangLeap\Levels\Level');
+		$intermediate = $intermediate->create(['code' => 'in', 'description'=>'Intermediate']);
+
+		$advanced = App::make('LangLeap\Levels\Level');
+		$advanced = $advanced->create(['code' => 'ad', 'description'=>'Advanced']);
+	}
+}
