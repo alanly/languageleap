e3d1f2688f33f9094074a598c3cf4d966cf2fb22
John Di Girolamo <jdigirolamo@hotmail.com>
2015-01-04 02:49:51 -0500

    Fixed sizing issue to be a bit more forgivable when the window is reduced to half the screen.

--- app/views/player/player.blade.php
+++ app/views/player/player.blade.php
@@ -12,41 +12,41 @@
 		</div>
 
 		<div class="row">
-			<div id="script" style="width: 50%;">
+			<div id="script" class="col-lg-6 col-md-6 col-xs-6" style="width: 50%;">
 			</div>
 
 			<!-- Player here -->
-			<div class="jumbotron" style="padding: 0; margin-bottom: 0;">	
+			<div class="jumbotron col-lg-6 col-md-6 col-xs-6" style="width: 50%;">	
 				<script src="/js/video-player.js"></script>	
-				<div id="player-container">
+				<div id="video-container">
 					<video width="100%" id="video-player" preload="none">
 						<source class="source" type="video/mp4">
 						<p>@lang('player.player.error')</p>
 					</video>
 
 					<!--Player controls-->
-					<div class="video-controls" style="width: 100%; font-size: 30px;">				
-						<a href="#" class="play-pause">
+					<div class="video-controls" style="width: 100%;">				
+						<a href="#" class="play-pause col-xs-1 col-md-1 col-lg-1">
 							<span class="glyphicon glyphicon-play"></span>
 						</a>
 
-						<a href="#" class="speed">
+						<a href="#" class="speed col-xs-1 col-md-1 col-lg-1">
 							<span class="glyphicon glyphicon-fast-forward"></span>
 						</a>
 
-						<div class="video-time">
+						<div class="video-time col-xs-3 col-md-3 col-lg-3">
 							<span class="current">0:00</span>/<span class="duration">0:00</span>
 						</div>
 						
-						<div id="slider" class="slider">
-							<input type="range" id="slider-bar" value="0" min="0" max="100"/>
+						<div id="slider" class="slider col-xs-5 col-md-5 col-lg-5">
+							<input type="range" id="slider-bar" class="slider-width" value="0" min="0" max="100"/>
 						</div>
 
-						<a href="#" class="full-screen">
+						<a href="#" class="full-screen col-xs-1 col-md-1 col-lg-1">
 							<span class="glyphicon glyphicon-fullscreen"></span>
 						</a>
 
-						<a href="#" class="mute">
+						<a href="#" class="mute col-xs-1 col-md-1 col-lg-1">
 							<span class="glyphicon glyphicon-volume-up"></span>
 						</a>
 					</div>
--- app/views/player/player2.blade.php
+++ app/views/player/player2.blade.php
@@ -10,39 +10,38 @@
 <div class="jumbotron" style="padding: 0; margin-bottom: 0;">
 	<script src="/js/video-player.js"></script>	
 	<div id="video-container">
-		<video width="100%" id="video-player">
-			<source src="../videos/TestVideo.mp4" type="video/mp4"/>
-			Your browser does not support the video tag.
+		<video width="100%" id="video-player" preload="none">
+			<source class="source" type="video/mp4">
+			<p>@lang('player.player.error')</p>
 		</video>
 
-		<div class="video-controls" style="width: 100%; font-size: 30px;">
-		
-			<a href="#" class="play-pause">
+		<!--Player controls-->
+		<div class="video-controls" style="width: 100%;">				
+			<a href="#" class="play-pause col-xs-1 col-md-1 col-lg-1">
 				<span class="glyphicon glyphicon-play"></span>
 			</a>
 
-			<a href="#" class="speed">
+			<a href="#" class="speed col-xs-1 col-md-1 col-lg-1">
 				<span class="glyphicon glyphicon-fast-forward"></span>
 			</a>
 
-			<div class="video-time">
+			<div class="video-time col-xs-3 col-md-3 col-lg-3">
 				<span class="current">0:00</span>/<span class="duration">0:00</span>
 			</div>
 			
-			<div id="slider" class="slider">
-				<input type="range" id="slider-bar" value="0" min="0" max="100"/>
+			<div id="slider" class="slider col-xs-5 col-md-5 col-lg-5">
+				<input type="range" id="slider-bar" class="slider-width" value="0" min="0" max="100"/>
 			</div>
 
-			<a href="#" class="full-screen">
+			<a href="#" class="full-screen col-xs-1 col-md-1 col-lg-1">
 				<span class="glyphicon glyphicon-fullscreen"></span>
 			</a>
 
-			<a href="#" class="mute">
+			<a href="#" class="mute col-xs-1 col-md-1 col-lg-1">
 				<span class="glyphicon glyphicon-volume-up"></span>
 			</a>
 		</div>
-
-	</div>
+	</div>			
 </div>
 
 @stop
