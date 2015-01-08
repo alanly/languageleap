390b47b2a348515e6af0ee54c76b888cea5e6e83
ThomasRahn <thomas@rahn.ca>
2015-01-08 18:07:27 -0500

    Converted to blade

--- app/views/admin/quiz/index.blade.php
+++ app/views/admin/quiz/index.blade.php
@@ -18,38 +18,32 @@
 			{{ Form::open(array('method' => 'put', 'action' => 'ApiQuizController@putCustomQuestion')) }}
 				<div class="form-group"/>
 				{{ Form::label('video_id', 'Video', array('class' => 'control-label')) }}
-			<?php
-				$dropdowns = array();
-				foreach($videos as $v)
-				{
-					$media = $v->viewable;
-					$dropdowns[$v->id] = $media->name;
-				}
-				
+				<select name="video_id" id="video_id">
+					@foreach($videos as $v)
+						<option id="{{ $v->id }}">{{ $v->viewable->name }}</option>
 
+					@endforeach
+				</select>
+				</div>
 				
-				echo Form::select('video_id', $dropdowns, array('class' => 'form-control'));
-				echo '</div>';
+				<div class="form-group"/>
+					{{ Form::label('question', 'Question', array('class' => 'control-label')) }}
+					{{ Form::text('question', null, array('class' => 'form-control', 'placeholder' => 'Insert question here')) }}
+				</div>
 				
-				echo '<div class="form-group"/>';
-				echo Form::label('question', 'Question', array('class' => 'control-label'));
-				echo Form::text('question', null, array('class' => 'form-control', 'placeholder' => 'Insert question here'));
-				echo '</div>';
+				<div class="form-group"/>
+				{{ Form::label('answer[]', 'Correct Answer', array('class' => 'control-label')) }}
+				{{ Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here')) }}
+				</div>
 				
-				echo '<div class="form-group"/>';
-				echo Form::label('answer[]', 'Correct Answer', array('class' => 'control-label'));
-				echo Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here'));
-				echo '</div>';
-				
-				echo '<div class="form-group"/>';
-				echo Form::label('answer[]', 'Other Answers', array('class' => 'control-label'));
-				echo Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here'));
-				echo Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here'));
-				echo Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here'));
-				echo '</div>';
-				
-				echo Form::submit('Save', array('class' => 'btn btn-success pull-right'));
-			?>
+				<div class="form-group"/>
+					{{ Form::label('answer[]', 'Other Answers', array('class' => 'control-label')) }}
+					{{ Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here')) }}
+					{{ Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here')) }}
+					{{ Form::text('answer[]', null, array('class' => 'form-control', 'placeholder' => 'Insert answer here')) }}
+				</div>
+			
+				{{ Form::submit('Save', array('class' => 'btn btn-success pull-right')) }}
 			{{ Form::close() }}
 			</div> <!-- panel-body -->
 		</div> <!-- panel-primary -->
