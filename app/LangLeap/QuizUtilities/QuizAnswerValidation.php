45a1e058da463cfef18a4dd2937ba056ac3914e9
alanly <hello@alan.ly>
2015-01-10 00:39:40 -0500

    Clean up code style.
    - Made sure the code followed the code styling guidelines.
    - Fixed some minor issues and removed un-necessary method overrides.
    
    LANGLEAP-74

--- app/LangLeap/QuizUtilities/QuizAnswerValidation.php
+++ app/LangLeap/QuizUtilities/QuizAnswerValidation.php
@@ -1,28 +1,23 @@
 <?php namespace LangLeap\QuizUtilities;
 
-use LangLeap\Core\UserInputResponse;
+use LangLeap\Accounts\User;
 use LangLeap\Core\InputDecorator;
+use LangLeap\Core\UserInputResponse;
 use LangLeap\Quizzes\Quiz;
 use LangLeap\Quizzes\VideoQuestion;
-use LangLeap\Accounts\User;
 
 /**
  * Concrete decorator that adds validation behavior to Quiz creation
  *
  * @author Quang Tran <tran.quang@live.com>
  */
-class QuizAnswerValidation extends InputDecorator
-{
-	function __construct(UserInputResponse $decoratedInputResponse)
-	{
-		parent::__construct($decoratedInputResponse);
-	}
+class QuizAnswerValidation extends InputDecorator {
 	
 	public function response($user_id, $input)
 	{
 		// Ensure that the Question exists, else return a 404.
 		$videoquestion = null;
-		if(isset($input['videoquestion_id']))
+		if (isset($input['videoquestion_id']))
 		{
 			$videoquestion_id = $input['videoquestion_id'];
 			$videoquestion = VideoQuestion::find($videoquestion_id);
@@ -49,28 +44,21 @@ class QuizAnswerValidation extends InputDecorator
 
 		// Ensure the quiz exists
 		$quiz = null;
-		if(isset($input['quiz_id']))
+		if (isset($input['quiz_id']))
 		{
 			$quiz_id = $input['quiz_id'];
 			$quiz = Quiz::find($quiz_id);
 		}
-		if(!$quiz)
+		if (! $quiz)
 		{
-			return [
-				'error',
-				"Quiz {$quiz_id} not found.",
-				404
-			];
+			return ['error', "Quiz {$quiz_id} not found.", 404];
 		}
-		else if($quiz->user_id != $user_id && !User::find($user_id)->is_admin)
+		else if ( ($quiz->user_id != $user_id) && ! User::find($user_id)->is_admin )
 		{
-			return [
-				'error',
-				'Not authorized to answer questions for quiz {$quiz_id}',
-				401
-			];
+			return ['error', "Not authorized to answer questions for quiz {$quiz_id}", 401];
 		}
 		
 		return parent::response($user_id, $input);
 	}
+
 }
