feecbdf969a7491a5263e98170bfdf64f45be494
Thomas <thomas@rahn.ca>
2015-01-11 09:54:51 -0500

    Formatting issues.

--- app/controllers/ApiUserLevelController.php
+++ app/controllers/ApiUserLevelController.php
@@ -2,14 +2,13 @@
 use LangLeap\Videos\Show;
 
 class ApiUserLevelController extends \BaseController {
-
-	    /**
-     * Show the level for the given user.
-     */
-    public function showLevel()
-    {
-        $user = Auth::user();
-
-        return View::make('account.level', ['level' => $user->level_id]);
-    }
+	/**
+	 * Show the level for the given user.
+	 */
+	public function showLevel()
+	{
+		$user = Auth::user();
+	
+		return View::make('account.level', ['level' => $user->level_id]);
+	}
 }
