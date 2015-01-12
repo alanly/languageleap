b2769ae31ace48ace9d162371cc5c9f2ac20165c
Alan Ly <me@alanly.ca>
2015-01-12 02:19:05 -0500

    Retrieve the correct answer from Question.
    - Add the "belongs to" association between a question and its correct
      answer. Allows us to immediately retrieve the correct answer.
    - Updated the associated test case.
    - Added default question ID for the `answers` table, otherwise we have a
      chicken-and-egg issue where we're unable to create an answer because we
      don't have a question, but we're unable to create a question because we
      don't have any answers.
    
    LANGLEAP-85

--- app/LangLeap/Quizzes/Question.php
+++ app/LangLeap/Quizzes/Question.php
@@ -1,13 +1,20 @@
 <?php namespace LangLeap\Quizzes;
 
 use Eloquent;
+
 /**
- * @author  Dror Ozgaon <Dror.Ozgaon@gmail.com>
+ * @author Dror Ozgaon <Dror.Ozgaon@gmail.com>
+ * @author Alan Ly <hello@alan.ly>
  */
 class Question extends Eloquent {
 
 	public    $timestamps = false;
 	protected $fillable   = ['answer_id', 'question'];
+
+	public function answer()
+	{
+		return $this->belongsTo('LangLeap\Quizzes\Answer');
+	}
 
 	public function answers()
 	{
@@ -18,4 +25,5 @@ class Question extends Eloquent {
 	{
 		return $this->hasMany('LangLeap\Quizzes\VideoQuestion');
 	}
+
 }
