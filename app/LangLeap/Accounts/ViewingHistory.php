3aa3099b6a7c3e0c79797950ceccdf2a444a3464
ThomasRahn <thomas@rahn.ca>
2015-01-18 15:34:10 -0500

    Added save and insert for viewing history

--- app/LangLeap/Accounts/ViewingHistory.php
+++ app/LangLeap/Accounts/ViewingHistory.php
@@ -7,11 +7,10 @@ class ViewingHistory extends Eloquent
 	public $timestamps = true;
 
 	protected $table = "viewing_history";
-	protected $fillable   = ['id', 'user_id', 'video_id', 'is_finished', 'current_time'];
+	protected $fillable   = ['user_id', 'video_id', 'is_finished', 'current_time'];
 	protected $rules = [
-		'id' => 'required',
-		'user_id' => 'required',
-		'video_id' => 'required',
+		'user_id' 	=> 'required',
+		'video_id' 	=> 'required',
 	];
 
 	public function video()
