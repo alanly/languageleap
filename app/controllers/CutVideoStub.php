024d6b83341b9b73968bc5415fc56af8300e7897
kc-wan <martinwan1992@hotmail.com>
2015-01-21 16:06:46 -0500

    LANGLEAP-138: Returns the same video depending on the number of segments from the input.

--- app/controllers/CutVideoStub.php
+++ app/controllers/CutVideoStub.php
@@ -6,6 +6,16 @@ class CutVideoStub extends \BaseController
 {
 	public function postIndex()
 	{
+		$videoToCut = Video::first();
+		$videoSegments = array();
 		
+		$segments = Input::get('segments');
+		
+		for ($i = 0; $i < count($segments); $i++)
+		{
+			$videoSegments[] = $videoToCut->toResponseArray();
+		}
+		
+		return $this->apiResponse("success", $videoSegments);
 	}
 }
