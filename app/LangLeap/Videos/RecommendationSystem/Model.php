7f1ab8bca22cf2c90151745d81f99a893cfac7a7
alanly <hello@alan.ly>
2015-01-23 15:58:31 -0500

    Interfaces and basic model.
    - Added the interfaces that will be necessary for the classification
      system.
    - Added the model that will contain our results of the user history
      analysis.
    
    [ci skip] because there's nothing to test yet.
    
    LANGLEAP-157

---
+++ app/LangLeap/Videos/RecommendationSystem/Model.php
@@ -0,0 +1,8 @@
+<?php namspace LangLeap\Videos\RecommendationSystem;
+
+/**
+ * Model represents the user's viewing preferences. It maintains a count of
+ * commonly occuring attributes from videos that the user has seen.
+ * @author Alan Ly <hello@alan.ly>
+ */
+class Model extends \stdClass {}
