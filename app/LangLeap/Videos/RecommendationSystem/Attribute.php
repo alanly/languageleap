c92a92dac0ebc5069074366a18a4cb54195b04ad
alanly <hello@alan.ly>
2015-01-23 16:57:44 -0500

    Add dep.inj. between Model and Attribute.
    - Added dependency injection for the Attribute class to Model so that we
      can mock the Attribute class for Model testing.
    
    [ci skip]
    
    LANGLEAP-157

---
+++ app/LangLeap/Videos/RecommendationSystem/Attribute.php
@@ -0,0 +1,59 @@
+<?php namespace LangLeap\Videos\RecommendationSystem;
+
+/**
+ * Attribute is a specific video attribute from the preference model's collection
+ * of attributes. Each attribute contains a set of residents of that attribute.
+ * For example, and "actors" attribute would contain a set of actors and the
+ * respective counter for each actor.
+ * @author Alan Ly <hello@alan.ly>
+ */
+class Attribute {
+
+	private $residents = [];
+
+
+	/**
+	 * Either adds a new resident to the attribute or increments an existing one.
+	 * In either case, the function returns the new count for that resident.
+	 * @param  string  $name  The name of the resident
+	 * @return int            The count of the addressed resident
+	 */
+	public function add($name)
+	{
+		// Initialize the resident if it is new.
+		if (! isset($this->residents[$name]))
+		{
+			$this->residents[$name] = 0;
+		}
+
+		return ++$this->residents[$name];
+	}
+
+
+	/**
+	 * Create a new Attribute instance.
+	 * @return Attribute The new attribute instance
+	 */
+	public function newInstance()
+	{
+		return new self();
+	}
+
+
+	/**
+	 * Returns the current count of the requested attribute resident. The value
+	 * represents the number of videos the user has seen, in which the attribute
+	 * resident occurs.
+	 * @param  string  $name  The name of the resident
+	 * @return int            The count of the requested resident
+	 */
+	public function count($name)
+	{
+		// If the resident does not exist, then return zero because that resident
+		// has occured zero times in the viewing history.
+		if (! isset($this->residents[$name])) return 0;
+
+		return $this->residents[$name];
+	}
+
+}
--- app/LangLeap/Videos/RecommendationSystem/Model.php
+++ app/LangLeap/Videos/RecommendationSystem/Model.php
@@ -5,4 +5,35 @@
  * commonly occuring attributes from videos that the user has seen.
  * @author Alan Ly <hello@alan.ly>
  */
-class Model extends \stdClass {}
+class Model {
+
+	/**
+	 * @var Attribute
+	 */
+	private $attribute;
+
+	private $attributes = [];
+
+
+	public function __construct(Attribute $attribute)
+	{
+		$this->attribute = $attribute;
+	}
+
+
+	/**
+	 * Magic method for retrieving a particular attribute.
+	 * @param  string  $name  The name of the attribute
+	 * @return Attribute
+	 */
+	public function __get($name)
+	{
+		if (! isset($this->attributes[$name]))
+		{
+			$this->attributes[$name] = $this->attribute->newInstance();
+		}
+
+		return $this->attributes[$name];
+	}
+
+}
