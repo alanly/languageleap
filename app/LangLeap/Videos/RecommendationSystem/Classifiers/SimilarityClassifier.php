749803e8a75d32e755081fdb851b8aa9c4a7021a
Alan Ly <me@alanly.ca>
2015-01-26 05:51:52 -0500

    Add similarity classifier.
    - Added a classifier algorithm that determines the similarity
      between a video's attributes and that of the user's profile.
    - This is a more appropriate algorithm than a Naive-Bayes due to the
      purpose of the system.
    
    LANGLEAP-157

---
+++ app/LangLeap/Videos/RecommendationSystem/Classifiers/SimilarityClassifier.php
@@ -0,0 +1,69 @@
+<?php namespace LangLeap\Videos\RecommendationSystem\Classifiers;
+
+use LangLeap\Videos\RecommendationSystem\Model;
+
+/**
+ * The SimilarityClassifier is a simple classification engine that determines
+ * whether the classifiable instance's attributes are similar to those
+ * defined by the reference model's.
+ * @author Alan Ly <hello@alan.ly>
+ */
+class SimilarityClassifier implements Classifier {
+
+	private $referenceModel;
+	private $classifyModel;
+
+
+	/**
+	 * Constructs a new instance.
+	 * @param Model $referenceModel The reference model to compare against
+	 * @param Model $classifyModel  The model that we want to classify
+	 */
+	public function __construct(Model $referenceModel, Model $classifyModel)
+	{
+		$this->referenceModel = $referenceModel;
+		$this->classifyModel = $classifyModel;
+	}
+
+
+	/**
+	 * Create a new classifier engine to classify the given model against the
+	 * reference model.
+	 * @param  Model $referenceModel The reference model to compare against
+	 * @param  Model $classifyModel  The model that we want to classify
+	 * @return SimilarityClassifier
+	 */
+	public static function create(Model $referenceModel, Model $classifyModel)
+	{
+		return new static($referenceModel, $classifyModel);
+	}
+
+
+	/**
+	 * Determines the probability that the classifiable instance will meet the
+	 * expectations outlined by the reference model.
+	 * @return float The likelihood that we will meet expectations
+	 */
+	public function classify()
+	{
+		$classifyAttributes = $this->classifyModel->toArray();
+		$totalWeight = 0;
+		$weight = 0;
+
+		foreach ($classifyAttributes as $a)
+		{
+			$refAttr = $this->referenceModel->{$a->getName()};
+			$totalWeight += $refAttr->weight();
+
+			foreach ($a->keys() as $name)
+			{
+				$weight += $refAttr->count($name);
+			}
+		}
+
+		if ($totalWeight === 0) return 0;
+
+		return (float)$weight / $totalWeight;
+	}
+
+}
---
+++ app/tests/Videos/RecommendationSystem/SimilarityClassifierTest.php
@@ -0,0 +1,105 @@
+<?php namespace LangLeap\Videos\RecommendationSystem\Classifiers;
+
+use LangLeap\TestCase;
+use LangLeap\Videos\RecommendationSystem\Model;
+use Mockery as m;
+
+class SimilarityClassifierTest extends TestCase {
+
+	protected function getModelMock()
+	{
+		return m::mock('LangLeap\Videos\RecommendationSystem\Model');
+	}
+
+
+	protected function getAttributeMock()
+	{
+		return m::mock('LangLeap\Videos\RecommendationSystem\Attribute');
+	}
+
+
+	public function testCreateReturnsNewInstance()
+	{
+		$model = $this->getModelMock();
+
+		$this->assertInstanceOf(
+			'LangLeap\Videos\RecommendationSystem\Classifiers\SimilarityClassifier',
+			SimilarityClassifier::create($model, $model)
+		);
+	}
+
+
+	public function testClassifyingTwoIdenticalModelsReturnsAnAppropriateResult()
+	{
+		$attribute = $this->getAttributeMock();
+		$attribute->shouldReceive('getName')->andReturn('foobar');
+		$attribute->shouldReceive('newInstance')->andReturn($attribute);
+		$attribute->shouldReceive('weight')->andReturn(1);
+		$attribute->shouldReceive('keys')->andReturn(['bar']);
+		$attribute->shouldReceive('count')->with('bar')->andReturn(1);
+
+		$model = new Model($attribute);
+		$model = m::mock($model);
+		$model->shouldReceive('toArray')->andReturn(['foobar' => $attribute]);
+
+		$classifier = SimilarityClassifier::create($model, $model);
+
+		$result = $classifier->classify();
+
+		$this->assertSame(1.0, round($result));
+	}
+
+	public function testClassifyingTwoDifferentModelsReturnsAnAppropriateResult()
+	{
+		// Ref. attribute
+		$attr1 = $this->getAttributeMock();
+		$attr1->shouldReceive('newInstance')->once()->andReturn($attr1);
+		$attr1->shouldReceive('weight')->once()->andReturn(1);
+		$attr1->shouldReceive('count')->once()->with('bar')->andReturn(0);
+
+		// Classifying attribute
+		$attr2 = $this->getAttributeMock();
+		$attr2->shouldReceive('getName')->once()->andReturn('foobar');
+		$attr2->shouldReceive('keys')->once()->andReturn(['bar']);
+
+		$model1 = new Model($attr1);
+
+		$model2 = new Model($attr2);
+		$model2 = m::mock($model2);
+		$model2->shouldReceive('toArray')->once()->andReturn(['foobar' => $attr2]);
+
+		$classifier = SimilarityClassifier::create($model1, $model2);
+
+		$result = $classifier->classify();
+
+		$this->assertNotSame(1.0, round($result));
+	}
+
+
+	public function testClassifyingTwoModelsWithAnEmptyReferenceModel()
+	{
+		// Ref. attribute
+		$attr1 = $this->getAttributeMock();
+		$attr1->shouldReceive('newInstance')->once()->andReturn($attr1);
+		$attr1->shouldReceive('weight')->once()->andReturn(0);
+		$attr1->shouldReceive('count')->once()->with('bar')->andReturn(0);
+
+		// Classifying attribute
+		$attr2 = $this->getAttributeMock();
+		$attr2->shouldReceive('getName')->once()->andReturn('foobar');
+		$attr2->shouldReceive('keys')->once()->andReturn(['bar']);
+
+		$model1 = new Model($attr1);
+
+		$model2 = new Model($attr2);
+		$model2 = m::mock($model2);
+		$model2->shouldReceive('toArray')->once()->andReturn(['foobar' => $attr2]);
+
+		$classifier = SimilarityClassifier::create($model1, $model2);
+
+		$result = $classifier->classify();
+
+		$this->assertSame(0, $result);
+	}
+	
+}
