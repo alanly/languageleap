6f943d9bd57ad59f90aab197f9ff5ffe8181bd65
Alan Ly <me@alanly.ca>
2015-01-27 01:09:09 -0500

    Cache model for iterative runs.
    - If we're performing consecutive classifications against the same
      historable model, then we should cache that model rather than generating
      a fresh on each consecutive iteration.
    
    LANGLEAP-157

--- app/LangLeap/Videos/RecommendationSystem/ClassificationDrivers/SimilarityClassificationDriver.php
+++ app/LangLeap/Videos/RecommendationSystem/ClassificationDrivers/SimilarityClassificationDriver.php
@@ -12,7 +12,10 @@ use LangLeap\Videos\RecommendationSystem\ClassificationEngines\SimilarityClassif
  */
 class SimilarityClassificationDriver implements ClassificationDriver {
 
-	protected $engine;
+	private $cacheHistorable = true;
+	private $engine;
+	private $historable;
+	private $historableModelCache;
 
 
 	public function __construct(SimilarityClassificationEngine $engine)
@@ -29,13 +32,11 @@ class SimilarityClassificationDriver implements ClassificationDriver {
 	 */
 	public function classify(Historable $historable, Classifiable $classifiable)
 	{
-		// Create the modeller instances for each parameter.
-		$userModeller = App::make('LangLeap\Videos\RecommendationSystem\Modellers\UserModeller', [$historable]);
-		$mediaModeller = App::make('LangLeap\Videos\RecommendationSystem\Modellers\MediaModeller', [$classifiable]);
+		// Get the user model.
+		$userModel = $this->modelHistorable($historable);
 
-		// Retrieve the respective models.
-		$userModel = $userModeller->model();
-		$mediaModel = $mediaModeller->model();
+		// Get the media model.
+		$mediaModel = $this->modelClassifiable($classifiable);
 
 		/**
 		 * The $userModel will act as the reference model to compare against.
@@ -43,6 +44,74 @@ class SimilarityClassificationDriver implements ClassificationDriver {
 		 */
 
 		return $this->engine->classify($userModel, $mediaModel);
+	}
+
+
+	/**
+	 * Gets the user model.
+	 * @param  Historable $historable The historable instance to generate the model for
+	 * @return LangLeap\Videos\RecommendationSystem\Model
+	 */
+	private function modelHistorable(Historable $historable)
+	{
+		// If this is the same historable instance and we have a cache of the model
+		// then just return the cached model so that we don't have to regenerate it.
+		
+		if (($historable === $this->historable)
+			&& isset($this->historableModelCache)
+			&& $this->cacheHistorable)
+		{
+			return $this->historableModelCache;
+		}
+
+		// Create the modeller instance.
+		$userModeller = App::make('LangLeap\Videos\RecommendationSystem\Modellers\UserModeller', [$historable]);
+
+		// Model the historable instance.
+		$userModel = $userModeller->model();
+
+		// Cache the values
+		if ($this->cacheHistorable)
+		{
+			$this->historable = $historable;
+			$this->historableModelCache = $userModel;
+		}
+
+		return $userModel;
+	}
+
+
+	/**
+	 * Gets the media model.
+	 * @param  Classifiable $classifiable The classifiable instance to model
+	 * @return LangLeap\Videos\RecommendationSystem\Model
+	 */
+	private function modelClassifiable(Classifiable $classifiable)
+	{
+		// Create the modeller.
+		$mediaModeller = App::make('LangLeap\Videos\RecommendationSystem\Modellers\MediaModeller', [$classifiable]);
+
+		// Model the instance.
+		return $mediaModeller->model();
+	}
+
+
+	/**
+	 * Enables the caching of the Historable model, so that we do not have to
+	 * regenerate it on consecutive runs of the same Historable. If set to false,
+	 * then the model will be created fresh on each run.
+	 * @param  boolean $enable Enable caching of the historable model
+	 */
+	public function enableHistorableCaching($enable = true)
+	{
+		$this->cacheHistorable = $enable;
+
+		// Clear out the cache if necessary
+		if (! $enable)
+		{
+			$this->historable = null;
+			$this->historableModelCache = null;
+		}
 	}
 	
 }
--- app/tests/Videos/RecommendationSystem/SimilarityClassificationDriverTest.php
+++ app/tests/Videos/RecommendationSystem/SimilarityClassificationDriverTest.php
@@ -9,6 +9,12 @@ use Mockery as m;
  */
 class SimilarityClassificationDriverTest extends TestCase {
 
+	public function tearDown()
+	{
+		parent::tearDown();
+		Mockery::close();
+	}
+
 	public function testEngineGetsCalledWithAppropriateParameters()
 	{
 		// Mock the historable and classifiable instances.
@@ -39,6 +45,77 @@ class SimilarityClassificationDriverTest extends TestCase {
 		$result = $driver->classify($historable, $classifiable);
 
 		$this->assertSame('foobar', $result);
+	}
+
+
+	public function testConsecutiveClassificationsAgainstSingleHistorableModelUsesCachedModel()
+	{
+		// Mock the historable and classifiable instances.
+		$historable = m::mock('LangLeap\Videos\RecommendationSystem\Historable');
+		$classifiable = m::mock('LangLeap\Videos\RecommendationSystem\Classifiable');
+
+		// Model the necessary models
+		$user = m::mock('LangLeap\Videos\RecommendationSystem\Model');
+		$media = m::mock('LangLeap\Videos\RecommendationSystem\Model');
+
+		// Mock the modellers for those instances.
+		$userModeller = m::mock('LangLeap\Videos\RecommendationSystem\Modellers\UserModeller');
+		$userModeller->shouldReceive('model')->andReturn($user)->once();
+
+		$mediaModeller = m::mock('LangLeap\Videos\RecommendationSystem\Modellers\MediaModeller');
+		$mediaModeller->shouldReceive('model')->andReturn($media)->times(4);
+
+		// Substitute the modeller instances into the autoloader.
+		App::instance('LangLeap\Videos\RecommendationSystem\Modellers\UserModeller', $userModeller);
+		App::instance('LangLeap\Videos\RecommendationSystem\Modellers\MediaModeller', $mediaModeller);
+		
+		// Mock the engine
+		$engine = m::mock('LangLeap\Videos\RecommendationSystem\ClassificationEngines\SimilarityClassificationEngine');
+		$engine->shouldReceive('classify')->times(4)->with($user, $media)->andReturn('foobar');
+
+		$driver = new SimilarityClassificationDriver($engine);
+
+		// Run the classifier four times.
+		$driver->classify($historable, $classifiable);
+		$driver->classify($historable, $classifiable);
+		$driver->classify($historable, $classifiable);
+		$driver->classify($historable, $classifiable);
+	}
+
+
+	public function testConsecutiveClassificationsAgainstSingleHistorableModelWithCachedDisabled()
+	{
+		// Mock the historable and classifiable instances.
+		$historable = m::mock('LangLeap\Videos\RecommendationSystem\Historable');
+		$classifiable = m::mock('LangLeap\Videos\RecommendationSystem\Classifiable');
+
+		// Model the necessary models
+		$user = m::mock('LangLeap\Videos\RecommendationSystem\Model');
+		$media = m::mock('LangLeap\Videos\RecommendationSystem\Model');
+
+		// Mock the modellers for those instances.
+		$userModeller = m::mock('LangLeap\Videos\RecommendationSystem\Modellers\UserModeller');
+		$userModeller->shouldReceive('model')->andReturn($user)->times(4);
+
+		$mediaModeller = m::mock('LangLeap\Videos\RecommendationSystem\Modellers\MediaModeller');
+		$mediaModeller->shouldReceive('model')->andReturn($media)->times(4);
+
+		// Substitute the modeller instances into the autoloader.
+		App::instance('LangLeap\Videos\RecommendationSystem\Modellers\UserModeller', $userModeller);
+		App::instance('LangLeap\Videos\RecommendationSystem\Modellers\MediaModeller', $mediaModeller);
+		
+		// Mock the engine
+		$engine = m::mock('LangLeap\Videos\RecommendationSystem\ClassificationEngines\SimilarityClassificationEngine');
+		$engine->shouldReceive('classify')->times(4)->with($user, $media)->andReturn('foobar');
+
+		$driver = new SimilarityClassificationDriver($engine);
+		$driver->enableHistorableCaching(false);
+
+		// Run the classifier four times.
+		$driver->classify($historable, $classifiable);
+		$driver->classify($historable, $classifiable);
+		$driver->classify($historable, $classifiable);
+		$driver->classify($historable, $classifiable);
 	}
 
 }
