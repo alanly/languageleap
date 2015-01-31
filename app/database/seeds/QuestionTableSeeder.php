285bec7c58060e53fba376ea3bcb978600cb73da
Dror Ozgaon <dror.ozgaon@gmail.com>
2015-01-31 18:35:51 -0500

    LANGLEAP-161 : Created seeds for the Question table.

--- app/database/seeds/QuestionTableSeeder.php
+++ app/database/seeds/QuestionTableSeeder.php
@@ -14,11 +14,21 @@ class QuestionTableSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 		$question = App::make('LangLeap\Questions\Question');
-		//TODO
-		//$question->create(["question" => "WHAT IS UP1?", "answer_id" => 1]);
-		//$question->create(["question" => "WHAT IS UP2?", "answer_id" => 2]);
-		//$question->create(["question" => "WHAT IS UP3?", "answer_id" => 3]);
-		//$question->create(["question" => "WHAT IS UP4?", "answer_id" => 4]);
+		$dq = App::make('LangLeap\Questions\DefinitionQuestion');
+		$cq = App::make('LangLeap\Questions\CustomQuestion');
+		$ddq = App::make('LangLeap\Questions\DragAndDropQuestion');
+		
+		$dq->question = "What is the definition of cat?";
+		$dq->save();
+		$question->create(["question_type" => "LangLeap\Questions\DefinitionQuestion", "question_id" => $dq->id, "answer_id" => 1]);
+
+		$cq->question = "What is the movie about?";
+		$cq->save();
+		$question->create(["question_type" => "LangLeap\Questions\CustomQuestion", "question_id" => $cq->id, "answer_id" => 2]);
+
+		$ddq->sentence = "The **BLANK** is scared of water.";
+		$ddq->save();
+		$question->create(["question_type" => "LangLeap\Questions\DragAndDropQuestion", "question_id" => $ddq->id, "answer_id" => 3]);
 	}
 	
 }
