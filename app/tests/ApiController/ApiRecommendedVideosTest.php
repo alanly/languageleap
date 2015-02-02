ff0f3d1a0ba9dbba3ddcb77e31b802cc40435ca8
Alan Ly <me@alanly.ca>
2015-02-01 20:18:55 -0500

    Add proper mocking to the test.

--- app/tests/ApiController/ApiRecommendedVideosTest.php
+++ app/tests/ApiController/ApiRecommendedVideosTest.php
@@ -17,6 +17,17 @@ class ApiRecommendedVideosControllerTest extends TestCase {
 		// Authenticate a user
 		$this->be(new User());
 
+		// Mock the Redis client.
+		$rc = Mockery::mock();
+		$rc->shouldReceive('zcard');
+		$rc->shouldReceive('pipeline');
+		$rc->shouldReceive('zrange');
+
+		$rf = Mockery::mock('Illuminate\Redis\Database');
+		$rf->shouldReceive('connection')->once()->andReturn($rc);
+
+		App::instance('Illuminate\Redis\Database', $rf);
+
 		$response = $this->action('GET', 'ApiRecommendedVideosController@index');
 
 		$this->assertResponseOk();
