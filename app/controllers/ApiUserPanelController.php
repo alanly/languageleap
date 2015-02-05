791edabec63945f35be064479d47a61ba8d527fd
John Di Girolamo <jdigirolamo@hotmail.com>
2015-02-04 19:15:23 -0500

    LANGLEAP-143: Got rid of useless background in the style. Set up controller to fetch user info.

--- app/controllers/ApiUserPanelController.php
+++ app/controllers/ApiUserPanelController.php
@@ -1,5 +1,6 @@
 <?php
 use LangLeap\Videos\Show;
+use LangLeap\Core\Language;
 
 class ApiUserPanelController extends \BaseController {
 	
@@ -20,6 +21,15 @@ class ApiUserPanelController extends \BaseController {
 	{
 		$user = Auth::user();
 	
-		return View::make('account.info')->with('level', $user->level()->first());
+		$data = ['fname' => $user->first_name, 'lname' => $user->last_name, 'lang' => $user->language_id, 'email' => $user->email];
+
+		$langs = Language::all();
+
+		foreach($langs as $lang)
+		{
+			$data[$lang->code] = ['language' => $lang->description];
+		}
+
+		return View::make('account.info', $data);
 	}
 }
