8910ca2745a8cfe905dce191041434ea7b6ca25e
Michael Lavoie <lavoie6453@gmail.com>
2015-02-14 18:39:08 -0500

    LANGLEAP-142: Added some tests for the implementation of the 'filterBy' function in the Show model.

--- app/tests/Videos/ShowTest.php
+++ app/tests/Videos/ShowTest.php
@@ -25,14 +25,88 @@ class ShowTest extends TestCase {
 		$season->save();
 		$this->assertCount(1, $show->seasons()->get());			
 	}
+
+	public function testFilterBySuccessWithNoSkip()
+	{
+		$this->seed();
+
+		$show = Show::first();
+
+		$input = [
+			'name' => $show->name,
+		];
+
+		$res = Show::filterBy($input, 1);
+
+		$this->assertCount(1, $res);
+		$this->assertSame($show->id, $res[0]->id);
+	}
+
+	public function testFilterBySuccessWithSkip()
+	{
+		$this->seed();
+
+		$show = Show::first();
+
+		// There should be more than one show beginning
+		// with the same letter
+		$letter = substr($show->name, 0, 1);
+
+		$input = [
+			'name' => $letter,
+		];
+
+		$res = Show::filterBy($input, 1, 1);
+
+		$match = Show::find($res[0]->id);
+		$matchFirstLetter = substr($match->name, 0, 1);
+
+		$this->assertCount(1, $res);
+		$this->assertSame($letter, $matchFirstLetter);
+	}
+
+	public function testFilterBySuccessWithIrrelevantQueryData()
+	{
+		$this->seed();
+
+		$show = Show::first();
+
+		$input = [
+			'name' => $show->name,
+			'irrelevant' => 'data',
+		];
+
+		$res = Show::filterBy($input, 1, 0);
+
+		$this->assertCount(1, $res);
+		$this->assertSame($show->id, $res[0]->id);
+	}
+
+	public function testFilterBySuccessWithTakeExtra()
+	{
+		$this->seed();
+
+		$show = Show::first();
+
+		// There should be more than one show beginning
+		// with the same letter
+		$input = [
+			'name' => substr($show->name, 0, 1),
+		];
+
+		$res = Show::filterBy($input, 2, 0);
+
+		$this->assertCount(2, $res);
+	}
+
 	protected function getShowInstance()
 	{
 		return App::make('LangLeap\Videos\Show');
 	}
+
 	protected function getSeasonInstance()
 	{
 		return App::make('LangLeap\Videos\Season');
 	}
-	
 
 }
