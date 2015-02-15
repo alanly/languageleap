18b18ab1e9b45450bc8d83bf6d8150e6f7c0a9b6
Michael Lavoie <lavoie6453@gmail.com>
2015-02-15 12:49:22 -0500

    LANGLEAP-142: Added basic functionality for filtering by using the search textbox.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -59,9 +59,9 @@
 	<div class="container-fluid">
 		<div class="row">
 			<ul class="nav nav-pills navbar-right" role="tablist">
-				<li role="presentation" class="active"><a href=".movies" aria-controls="movies" role="tab" data-toggle="tab" onclick="loadMovies();">@lang('index.layout.tabs.movies')</a></li>
-				<li role="presentation"><a href=".shows" aria-controls="shows" role="tab" data-toggle="tab" onclick="loadShows();">@lang('index.layout.tabs.shows')</a></li>
-				<li role="presentation"><a href=".commercials" aria-controls="commercials" role="tab" data-toggle="tab" onclick="loadCommercials();">@lang('index.layout.tabs.commercials')</a></li>
+				<li role="presentation" class="active"><a href=".movies" aria-controls="movies" role="tab" data-toggle="tab">@lang('index.layout.tabs.movies')</a></li>
+				<li role="presentation"><a href=".shows" aria-controls="shows" role="tab" data-toggle="tab">@lang('index.layout.tabs.shows')</a></li>
+				<li role="presentation"><a href=".commercials" aria-controls="commercials" role="tab" data-toggle="tab">@lang('index.layout.tabs.commercials')</a></li>
 			</ul>
 			<div class="filters-toggle pull-left">
 				<span class="hide-filters-text">
@@ -147,15 +147,15 @@
 			</div>
 			<div role="tabpanel" class="content col-md-10">
 				<div class="tab-content">
-					<div role="tabpanel" class="tab-pane active section movies">
+					<div role="tabpanel" class="tab-pane active section movies" data-type="movie">
 						<div class="elements movie-elements">
 						</div>
 					</div>
-					<div role="tabpanel" class="tab-pane section shows">
+					<div role="tabpanel" class="tab-pane section shows" data-type="show">
 						<div class="elements show-elements">
 						</div>
 					</div>
-					<div role="tabpanel" class="tab-pane section commercials">
+					<div role="tabpanel" class="tab-pane section commercials" data-type="commercial">
 						<div class="elements commercial-elements">
 						</div>
 					</div>
@@ -166,6 +166,187 @@
 
 	<script type="text/javascript">
 
+		/**
+		* Given a type (ie. movie), will build a 
+		* query in JSON format suited for that type.
+		*/
+		function buildQueryData(type) {
+			var typeQueryData;
+			var commonQueryData = getCommonQueryData();
+
+			if (type == 'movie')
+				typeQueryData = getMovieQueryData();
+			else if (type == 'show')
+				typeQueryData = getShowQueryData();
+			else
+				typeQueryData = getCommercialQueryData();
+
+			// Merge param2 into param1
+			$.extend(commonQueryData, typeQueryData);
+
+			return commonQueryData;
+		}
+
+
+		/**
+		* Returns a JSON of query data that
+		* represents the 'movie' type.
+		*/
+		function getMovieQueryData() {
+			var searchText = getSearchText();
+			
+			return {
+				'type': 'movie',
+				'director': searchText,
+				'actor': searchText,
+				'genre': searchText
+			};
+		}
+
+		/**
+		* Returns a JSON of query data that
+		* represents the 'show' type.
+		*/
+		function getShowQueryData() {
+			var searchText = getSearchText();
+			
+			return {
+				'type': 'show',
+				'director': searchText
+			};
+		}
+
+		/**
+		* Returns a JSON of query data that
+		* represents the 'commercial' type.
+		*/
+		function getCommercialQueryData() {
+			var searchText = getSearchText();
+			
+			return {
+				'type': 'commercial'
+			};
+		}
+
+		/**
+		* Returns a JSON of query data that is
+		* common to all types.
+		*/
+		function getCommonQueryData() {
+			var searchText = getSearchText();
+
+			return {
+				'take': 10,
+				'skip': 0,
+				'name': searchText
+			};
+		}
+
+		/**
+		* Retrieves the text that is in the searchbox.
+		*/
+		function getSearchText() {
+			return $('#search input').val();
+		}
+
+		/**
+		* Retrieves the string type to be sent to the server
+		* for filtering.
+		*/
+		function getActiveTabType() {
+			return $('.tab-content .active').data('type');
+		}
+
+		/**
+		* Returns the panel with the given type as a
+		* jQuery object.
+		*/
+		function getPanelWithType(type) {
+			return $('.tab-content [data-type=' + type + ']');
+		}
+
+		/**
+		* A timer is used to reduce the number of requests
+		* to the server.
+		*/
+		window.filterTimeout = null;
+		function onSearchKeypress() {
+			clearTimeout(filterTimeout);
+			filterTimeout = setTimeout(refreshFilteredData, 600);
+		}
+
+		/**
+		* This function is called when the user switches panels.
+		*/
+		function onSwitchPanel() {
+			refreshFilteredData();
+		}
+
+		/**
+		* Will refresh the content to reflect the currently
+		* set filters.
+		*/
+		function refreshFilteredData() {
+			var type = getActiveTabType();
+			var data = buildQueryData(type);
+
+			$.ajax({
+				type: 'GET',
+				data: data,
+				dataType: 'JSON',
+				url: '/api/metadata/filter',
+				success: onGetFilteredSuccess(type),
+				error: onGetFilteredFail
+			});
+		}
+
+		/**
+		* This function is called upon successful
+		* interaction with the server for filtered results.
+		*/
+		function onGetFilteredSuccess(type) {
+			return function(data) {
+				var $panel = getPanelWithType(type);
+
+				// Empty the panel
+				$panel.children('.elements').empty();
+
+				$.each(data.data, function(i, v) {
+					addElementToPanel($panel, type, v);
+				});
+			};
+		}
+
+		/**
+		* This function is called when filtered results
+		* retrieval has failed.
+		*/
+		function onGetFilteredFail(data) {
+			var message = data.responseJSON.data;
+
+			if(message === undefined)
+				message = 'There was a problem loading the information, Please try again at a later time.';
+			
+			var error = '<div class="alert alert-danger" role="alert">' + message + '</div>';
+			$('.tab-content .active .elements').html(error);
+		}
+
+		/**
+		* Constructs an element with the given type and data
+		* and adds it to the given panel.
+		*/
+		function addElementToPanel($panel, type, elementData) {
+			var url = '/' + type + '/' + elementData.id;
+			var coverUrl = (elementData.cover) ? elementData.cover : 'http://placehold.it/180x240';
+			var element =	'<div class="element" id="' + elementData.id + '">' +
+								'<a href="' + url + '" class="thumbnail cover" data-toggle="popover" data-trigger="hover" data-placement="auto" title="' + elementData.name + '" data-content="' + elementData.description + '">' +
+									'<img src="' + coverUrl + '" />' +
+								'</a>' +
+							'</div>';
+
+			$panel.children('.elements').append(element);
+		}
+
 		function toggleFilters() {
 			$('.filters').animate({ width: 'toggle' }, 350);
 			$('.hide-filters-text').animate({ width: 'toggle' }, 350);
@@ -173,9 +354,22 @@
 			$('.content').toggleClass('col-md-10');
 		}
 
+		function loadInitialContent() {
+			refreshFilteredData();
+		}
+
 		$(document).ready(function() {
-			loadMovies();
-			$('.filters-toggle').click(toggleFilters);
+			// Load the content for the active panel
+			loadInitialContent();
+
+			// The button for toggling the filters
+			$('.filters-toggle').on('click', toggleFilters);
+
+			// Handler for when the user types in the search box
+			$('#search input').on('keydown', onSearchKeypress);
+
+			// Handler for when the user switches panel
+			$(document).on('shown.bs.tab', onSwitchPanel);
 		});
 
 		function loadMovies()
