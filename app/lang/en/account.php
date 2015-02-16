2e332d22b4788c735af75e931da21e15f6167062
John Di Girolamo <jdigirolamo@hotmail.com>
2015-02-15 19:15:46 -0500

    LANGLEAP-172: Added @lang to user info page.

---
+++ app/lang/en/account.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+	
+	/*
+	|------------------------------------------------------------------
+	| Account Language Lines.
+	|------------------------------------------------------------------
+	|
+	| The following language lines will be used for the english 
+	| version of the player page.
+	|
+	*/
+
+	'info' => array(
+		'title' => 'Personal Information',
+		'subtitle' => 'Review and change your personal information.',
+		'required' => 'Required Field',
+		'fname' => 'Continue to Quiz',
+		'lname' => 'Define Selected',
+		'email' => 'Enter New Email',
+		'lang' => 'Select Language',
+		'pass' => 'Enter New Password',
+		'pass2' => 'Confirm New Password',
+		'cpass' => 'Enter your current password to confirm changes',
+		'submit' => 'Submit',
+	),
+	'level' => array(
+		'level' => 'My Level',
+		'about' => 'About',
+		'contact' => 'Contact',
+	),
+);
