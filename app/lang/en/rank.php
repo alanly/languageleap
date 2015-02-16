283286c5fff7839fd9426b834e859ca38eb9e9f0
ThomasRahn <thomas@rahn.ca>
2015-02-16 17:03:11 -0500

    LANGLEAP-172: internationalized ranked tutorial video

---
+++ app/lang/en/rank.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+	'quiz' => array(
+		'skip' => 'Skip Ranking Process',
+		'intro' => 'Let\'s see how good you are!',
+		'desc' => 'This ranking procedure let\'s us figure out how much you already know,
+			so that we can tailor lessons to your needs.',
+		'submit' => 'Submit Answers',
+	),	
+	'rank' => array(
+		'self' => 'Your Ranking',
+		'quizSkip' => 'Skipping the ranking process will automatically rank you as a beginner.',
+	),
+	'player' => array(
+		'faster' => 'Faster',
+		'slower' => 'Slower',
+		'normal' => 'Normal',
+	), 
+	'proceed' => 'Proceed to Quiz',
+	'title' => 'Tutorial Video',
+);
