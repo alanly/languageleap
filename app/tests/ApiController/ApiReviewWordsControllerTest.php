4fabbf3329b8702fd0c3b6449ec6f5b2eca9f3e4
ThomasRahn <thomas@rahn.ca>
2015-02-18 11:47:53 -0500

    Fixed test: using wrong method name, and no user was set

--- app/tests/ApiController/ApiReviewWordsControllerTest.php
+++ app/tests/ApiController/ApiReviewWordsControllerTest.php
@@ -2,14 +2,16 @@
 
 use LangLeap\TestCase;
 use LangLeap\Words\WordBank;
+use LangLeap\Accounts\User;
 
 class ApiReviewWordsControllerTest extends TestCase {
 
 	public function testIndex()
 	{
 		$this->seed();
-		
-		$response = $this->action('GET', 'ApiReviewWordsController@index');
+		$this->be(User::first());
+
+		$response = $this->action('GET', 'ApiReviewWordsController@getIndex');
 		$this->assertResponseOk();
 		$this->assertJson($response->getContent());
 	}
