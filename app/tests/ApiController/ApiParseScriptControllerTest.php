d366808d801760f852ea0f246976614fd3d65025
ThomasRahn <thomas@rahn.ca>
2015-02-21 12:21:48 -0500

    LANGLEAP-174: Added Tests for parse script api

---
+++ app/tests/ApiController/ApiParseScriptControllerTest.php
@@ -0,0 +1,47 @@
+<?php 
+
+use LangLeap\TestCase;
+
+/**
+*
+* @author Thomas Rahn <Thomas@rahn.ca>
+*
+*/
+class ApiParseScriptControllerTest extends TestCase {
+
+	public function testIndex()
+	{
+		$this->seed();
+
+		$script = new Symfony\Component\HttpFoundation\File\UploadedFile(Config::get('media.test') . DIRECTORY_SEPARATOR . 'test-script.srt', 'test-script.srt');
+
+		$response = $this->action(
+			'POST',
+			'ApiParseScriptController@postIndex',
+			[],
+			[],
+			['script-file' => $script]
+		);
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertResponseOk();
+	}
+
+	public function testIndexWithInvalidFileType()
+	{
+		$this->seed();
+
+		$script = new Symfony\Component\HttpFoundation\File\UploadedFile(Config::get('media.test') . DIRECTORY_SEPARATOR . '1.txt', '1.txt');
+
+		$response = $this->action(
+			'POST',
+			'ApiParseScriptController@postIndex',
+			[],
+			[],
+			['script-file' => $script]
+		);
+
+		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
+		$this->assertResponseStatus(400);
+	}
+}
