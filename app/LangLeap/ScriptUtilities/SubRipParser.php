d40b42e1c5fc9b8a36fc790d47e0acd5ec6861ac
Michael Lavoie <lavoie6453@gmail.com>
2015-02-21 22:00:27 -0500

    LANGLEAP-175: Added handling for SRT files that may have special tags (eg. <font>, {i}) and DVD rectangle positioning (eg. X1:53 X2:303 Y1:438 Y2:453).

--- app/LangLeap/ScriptUtilities/SubRipParser.php
+++ app/LangLeap/ScriptUtilities/SubRipParser.php
@@ -10,16 +10,26 @@ class SubRipParser implements Parser {
 		if (! $input || ! is_string($input))
 			return null;
 
-		// Example of a pattern that will match:
+		// Strips all html type tags
+		$parsed = strip_tags($input);
+
+		// Strips all tags such as {i}{/i}, {u}{/u}, etc...
+		$parsed = preg_replace('/{.*?}/', '', $parsed);
+
+		// Example of patterns that will match:
 		//		1
 		//		00:00:02,810 --> 00:00:06,002
-		$pattern = '/\d+\s*\n\s*(\d{2}:\d{2}:\d{2}),\d{3}\s-->\s\d{2}:\d{2}:\d{2},\d{3}/';
+		//					or
+		//		1
+		//		00:00:02,810 --> 00:00:06,002  X1:53 X2:303 Y1:438 Y2:453
+		$pattern = '/\d+\s*\n\s*(\d{2}:\d{2}:\d{2}),\d{3}\s-->\s\d{2}:\d{2}:\d{2},\d{3}((\s+.\d:\d+){4})?/';
 
-		// The pattern example above with this replacement will produce the following:
+		// The pattern examples above with this replacement will produce the following:
 		//		<span data-type="actor" data-timestamp="00:00:02">Speaker</span>
 		$replacement = '<span data-type="actor" data-timestamp="${1}">Speaker</span>';
 
-		$parsed = preg_replace($pattern, $replacement, $input);
+		// Parse the timestamps to the correct format
+		$parsed = preg_replace($pattern, $replacement, $parsed);
 
 		// All line-breaks are replaced by <br> tags to give a clean presentation of the
 		// subtitles to the admin
--- app/tests/ScriptUtilities/SubRipParserTest.php
+++ app/tests/ScriptUtilities/SubRipParserTest.php
@@ -10,14 +10,14 @@ class SubRipParserTest extends TestCase {
 	protected function getSubtitleText()
 	{
 		return	"1\n" .
-				"00:00:02,810 --> 00:00:06,002\n" .
+				"00:00:02,810 --> 00:00:06,002 X1:53 X2:303 Y1:438 Y2:453\n" .
 				"Kids, Im gonna tell you an incredible story\n\n" .
 				"2\n" .
 				"00:00:06,002 --> 00:00:08,263\n" .
-				"the story of how I met your mother.\n\n" .
+				"<font>the story of <i>how I met</i> your mother.</font>\n\n" .
 				"3\n" .
 				"00:00:08,263 --> 00:00:10,551\n" .
-				"Are we being punished for something ?\n\n";
+				"{u}Are we being punished for something ?{/u}\n\n";
 	}
 
 	protected function getExpectedParsedText()
