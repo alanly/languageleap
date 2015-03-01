86008527241b9f36b9866600c32a2c7e130747e0
Michael Lavoie <lavoie6453@gmail.com>
2015-03-01 17:51:03 -0500

    Localized admin/quiz/index.

--- app/lang/en/admin.php
+++ app/lang/en/admin.php
@@ -40,4 +40,18 @@ return array(
 	'upload' => array(
 		'success' => 'Media Uploaded Successfully'
 	),
+	'quiz' => array(
+		'insert' => array(
+			'heading' => 'Insert Custom Quiz',
+			'video_label' => 'Video',
+			'question_label' => 'Question',
+			'correct_answer_label' => 'Correct Answer',
+			'other_answers_label' => 'Other Answers',
+			'save_button' => 'Save',
+			'save_success' => 'Save Successful',
+			'save_fail' => 'Save Failed',
+			'question_placeholder' => 'Insert question here',
+			'answer_placeholder' => 'Insert answer here',
+		),
+	),
 );
