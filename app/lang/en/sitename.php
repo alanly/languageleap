868c79bd54cee7fcee8a06c45dd8809dab0623fb
John Di Girolamo <jdigirolamo@hotmail.com>
2015-03-01 15:45:19 -0500

    Added @lang for the site's name in case it's to be translated or changed in the future.

---
+++ app/lang/en/sitename.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	
+	/*
+	|------------------------------------------------------------------
+	| Name of the website.
+	|------------------------------------------------------------------
+	|
+	| The following language line will be used for the english 
+	| version of the website's name.
+	|
+	*/
+
+	'name' => array(
+		'name' => 'LanguageLeap',
+	),
+);
