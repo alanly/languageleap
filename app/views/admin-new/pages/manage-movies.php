114eb447929cba2031f2f86218eaa3c625b18d98
ThomasRahn <thomas@rahn.ca>
2015-03-07 17:49:45 -0500

    Initial controller and view for movies using angular

--- app/views/admin-new/pages/manage-movies.php
+++ app/views/admin-new/pages/manage-movies.php
@@ -1,1 +1,21 @@
 <h1>Manage Movies</h1>
+
+
+<table class="table table-hover movies">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Action</th>
+			<th>Name</th>
+			<th>Publish</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr ng-repeat="movie in movies">
+			<td class="id">{{ movie.id }}</td>
+			<td><a href="#"><i class="fa fa-edit fa-fw"></i></a><a href="#"><i class="fa fa-trash fa-fw"></i></a></td>
+			<td>{{ movie.name }}</td>
+			<td><input class="publish" type="checkbox" ng-checked="movie.is_published" /></td>
+		</tr>
+	</tbody>
+</table>
--- public/js/admin/controllers/movie/MovieController.js
+++ public/js/admin/controllers/movie/MovieController.js
@@ -4,7 +4,16 @@
 
 	app.controller('MovieController', ['$scope', '$log', '$routeParams', '$http', function($scope, $log, $routeParams, $http) {
 
+		$scope.movies = [];
 
-	}]);
+		// Load all of the movies
+		$http.get('/api/metadata/movies').
+			success(function(data) {
+				//get the movies from the server and set it.
+				$scope.movies = data.data;
+			});
+		}]);
+
+	
 
 })();
