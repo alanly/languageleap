0b9a44f8ea993b01fd649ca738be62e684e1a3e1
Michael Lavoie <lavoie6453@gmail.com>
2015-03-07 19:16:19 -0500

    Added a show table that displays all TV shows and added a publish checkbox to publish/unpublish  shows.

--- app/views/admin-new/pages/manage-shows.php
+++ app/views/admin-new/pages/manage-shows.php
@@ -1,1 +1,20 @@
 <h1>Manage TV Shows</h1>
+
+<table class="table table-hover shows">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Action</th>
+			<th>Name</th>
+			<th>Publish</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr ng-repeat="show in shows">
+			<td class="id">{{ show.id }}</td>
+			<td><a href="#"><i class="fa fa-edit fa-fw"></i></a><a href="#"><i class="fa fa-trash fa-fw"></i></a></td>
+			<td>{{ show.name }}</td>
+			<td><input class="publish" type="checkbox" ng-model="show.is_published" ng-checked="show.is_published" ng-click="onPublishClick($event, show)"></td>
+		</tr>
+	</tbody>
+</table>
--- public/js/admin/controllers/show/ShowController.js
+++ public/js/admin/controllers/show/ShowController.js
@@ -4,6 +4,24 @@
 
 	app.controller('ShowController', ['$scope', '$log', '$routeParams', '$http', function($scope, $log, $routeParams, $http) {
 
+		$scope.shows = [];
+
+		// Load all of the shows
+		$http.get('/api/metadata/shows').
+			success(function(data) {
+				$scope.shows = data.data;
+			});
+
+		// Publish checkbox handler
+		$scope.onPublishClick = function($event, show) {
+			show.is_published = $event.target.checked;
+			$scope.updateShow(show);
+		};
+
+		// Update a given show
+		$scope.updateShow = function(show) {
+			$http.put('/api/metadata/shows/' + show.id, show);
+		};
 
 	}]);
 
