cb0f40d929c59938707ff1836ca6186c4c2b2a76
Alan Ly <me@alanly.ca>
2015-03-18 13:13:39 -0400

    Created updater utility for media images.
    - Pulled the image updating implementation into its own reusable utility.
      Used via an observer pattern implementation. Anything that's file
      related requires a pretty extensive implementation so this would help
      out in the future-maintenance-department.

---
+++ app/LangLeap/VideoUtilities/MediaUpdaterListener.php
@@ -0,0 +1,28 @@
+<?php namespace LangLeap\VideoUtilities;
+
+use LangLeap\Videos\Media;
+
+/**
+ * Defines the interface that clients listening to update operations on Media
+ * instances must implement.
+ * @author Alan Ly <hello@alan.ly>
+ */
+interface MediaUpdaterListener {
+
+  /**
+   * Handle the event that the Media instance has been successfully updated.
+   * @param  Media  $media the Media instance that has been updated.
+   * @return mixed
+   */
+  public function mediaUpdated(Media $media);
+
+
+  /**
+   * Handle the event that the attempt to update the Media instance results in
+   * validation errors.
+   * @param  mixed $errors a collection of error messages from the validator.
+   * @return mixed
+   */
+  public function mediaValidationError($errors);
+  
+}
