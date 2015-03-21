ca96607b181b2a7a814a44d7f3b5ea3028ca6596
Michael Lavoie <lavoie6453@gmail.com>
2015-03-21 11:53:31 -0400

    Changed the input type to number for the season number and episode number form inputs.

--- app/views/admin-new/partials/edit-season-modal.php
+++ app/views/admin-new/partials/edit-season-modal.php
@@ -6,7 +6,7 @@
 		<div class="col-md-12">
 			<div class="form-group">
 				<label for="number"><?= Lang::get('admin.modal.edit_media.season_number') ?></label>
-				<input type="text" class="form-control" id="number" placeholder="<?= Lang::get('admin.form.placeholders.season.number') ?>" ng-model="season.number">
+				<input type="number" class="form-control" id="number" placeholder="<?= Lang::get('admin.form.placeholders.season.number') ?>" ng-model="season.number">
 			</div>
 			<div class="form-group">
 				<label for="description"><?= Lang::get('admin.modal.description') ?></label>
