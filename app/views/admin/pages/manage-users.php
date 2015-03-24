10030ea3666c66c842245176ae5e33659df2cd16
Michael Lavoie <lavoie6453@gmail.com>
2015-03-23 21:24:57 -0400

    Added sorting to the manage users table.

--- app/views/admin/pages/manage-users.php
+++ app/views/admin/pages/manage-users.php
@@ -1,17 +1,17 @@
 <h1>Manage Users</h1>
 
 <table class="table table-hover users" ng-if="users.length !== 0">
-	<thead>
+	<thead ng-init="orderByReverse = false; orderByPredicate = 'username';">
 		<tr>
-			<th>ID</th>
-			<th>Actions</th>
-			<th>UserName</th>
-			<th>Email</th>
-			<th>Active</th>
+			<th class="col-md-1">ID</th>
+			<th class="col-md-2">Actions</th>
+			<th class="col-md-3"><span class="sortable" ng-click="orderByReverse = !orderByReverse; orderByPredicate = 'username'">UserName<i ng-class="{ 'fa fa-caret-up fa-fw': (orderByPredicate == 'username'), 'fa-flip-vertical': !orderByReverse }"></i></span></th>
+			<th class="col-md-5"><span class="sortable" ng-click="orderByReverse = !orderByReverse; orderByPredicate = 'email'">Email<i ng-class="{ 'fa fa-caret-up fa-fw': (orderByPredicate == 'email'), 'fa-flip-vertical': !orderByReverse }"></i></span></th>
+			<th class="col-md-1"><span class="sortable" ng-click="orderByReverse = !orderByReverse; orderByPredicate = 'is_active'">Active<i ng-class="{ 'fa fa-caret-up fa-fw': (orderByPredicate == 'is_active'), 'fa-flip-vertical': !orderByReverse }"></i></span></th>
 		</tr>
 	</thead>
 	<tbody>
-		<tr ng-repeat="user in users">
+		<tr ng-repeat="user in users | orderBy:orderByPredicate:orderByReverse">
 			<td class="id">{{ user.id }}</td>
 			<td><a ng-click="remove(user);"><i class="fa fa-trash fa-fw"></i></a></td>
 			<td>{{ user.username }}</td>
