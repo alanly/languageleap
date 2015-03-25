72114df0ab8661028a15f0fde57dde4688818f2d
Alan Ly <me@alanly.ca>
2015-03-25 10:42:58 -0400

    Font-Awesome was included twice...
    [ci skip]

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -8,7 +8,6 @@
 
 		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
 
 		<link rel="stylesheet" href="/css/layout.css">
 
