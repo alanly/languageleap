690c92c8c0ba3c3bdd312409a3d5383699207a75
Alan Ly <hello@alan.ly>
2015-03-25 15:31:34 -0400

    Update DictionaryFactory so that it can be mocked.
    - Added the ability to introduce a mock dictionary adapter as well as
      an entire mock factory instance so that we can test the
      implementation without having to actually call an external API
      service. Which to me feels like it should be a basic consideration.
      But I guess people fuck up sometimes.

--- app/tests/ApiController/ApiDictionaryTest.php
+++ app/tests/ApiController/ApiDictionaryTest.php
@@ -2,13 +2,42 @@
 
 use LangLeap\TestCase;
 use LangLeap\Words\Definition;
+use LangLeap\DictionaryUtilities\DictionaryFactory;
 
 
-class ApiDictionaryTest extends TestCase 
-{
+class ApiDictionaryTest extends TestCase {
+
+	private function getDefinitionMock()
+	{
+		return Mockery::mock('LangLeap\Words\Definition');
+	}
+
+	private function getAdapterMock()
+	{
+		return Mockery::mock('LangLeap\DictionaryUtilities\IDictionaryAdapter');
+	}
+
+	private function getFactoryMock()
+	{
+		return Mockery::mock('LangLeap\DictionaryUtilities\DictionaryFactory');
+	}
 
 	public function testWordDefinition()
 	{
+		// Mock the definition instance.
+		$definition = $this->getDefinitionMock();
+		$definition->shouldReceive('toResponseArray')->once()->andReturn(['word' => 'foobar']);
+
+		// Mock the dictionary adapter.
+		$adapter = $this->getAdapterMock();
+		$adapter->shouldReceive('getDefinition')->once()->andReturn($definition);
+		
+		// Mock the factory.
+		$factory = $this->getFactoryMock();
+		$factory->shouldReceive('getDictionary')->once()->andReturn($adapter);
+
+		DictionaryFactory::getInstance($factory);
+
 		$this->seed();
 		$response = $this->action('GET', 'ApiDictionaryController@index', [], ['word' => 'dog', 'video_id' => '1']);
 
@@ -16,20 +45,29 @@ class ApiDictionaryTest extends TestCase
 		$this->assertResponseOk();
 
 		$data = $response->getData()->data;
-
-		$this->assertObjectHasAttribute('definition', $data);
-		$this->assertObjectHasAttribute('audio_url', $data);
+		
+		$this->assertSame('foobar', $data->word);
 	}
 
 	public function testWordDefinitionNotFound()
 	{
+		// Mock the dictionary adapter.
+		$adapter = $this->getAdapterMock();
+		$adapter->shouldReceive('getDefinition')->once()->andReturn(null);
+		
+		// Mock the factory.
+		$factory = $this->getFactoryMock();
+		$factory->shouldReceive('getDictionary')->once()->andReturn($adapter);
+
+		DictionaryFactory::getInstance($factory);
+
 		$this->seed();
 		$response = $this->action('GET', 'ApiDictionaryController@index', [], ['word' => 'abcdef', 'video_id' => '1']);
 
+		$data = $response->getData()->data;
+
 		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
 		$this->assertResponseStatus(404);
-
-		$data = $response->getData()->data;
 
 		$this->assertSame('Definition not found.', $data);
 	}
@@ -42,4 +80,5 @@ class ApiDictionaryTest extends TestCase
 		$this->assertInstanceOf('Illuminate\Http\JsonResponse', $response);
 		$this->assertResponseStatus(404);
 	}
+
 }
